654b56b069ab6c072eae4c8cad0dce5d234f0a31
Anna An <annaan.golf@gmail.com>
2021-09-07 12:43:57 +1200

    solution added

--- 684.redundant-connection.js
+++ 684.redundant-connection.js
@@ -1,4 +1,5 @@
 // 684. Redundant Connection
+// Frequency: 23.64%
 // In this problem, a tree is an undirected graph that is connected and has no cycles.
 // You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
 // Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
@@ -40,8 +41,8 @@
   // find(1) = find(2) = find(3) = find(4) = 4, find(4) = 4.
   // find(1) === find(4), so return edge ([1, 4])
 
-// Time Complexity: O() 88ms
-// Space Complexity: O() 41.8MB
+// Time Complexity: O(n) 88ms
+// Space Complexity: O(n) 41.8MB
   var findRedundantConnection = function(edges) {
     let uf = {};
     for (var edge of edges) {
