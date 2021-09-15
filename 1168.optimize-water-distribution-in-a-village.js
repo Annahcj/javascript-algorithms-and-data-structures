7ad6086d9a6dfc281a33920cfc06a43d1ac0b581
Anna An <annaan.golf@gmail.com>
2021-09-15 12:56:03 +1200

    solution added

--- 1168.optimize-water-distribution-in-a-village.js
+++ 1168.optimize-water-distribution-in-a-village.js
@@ -79,6 +79,112 @@ class UnionFind {
   }
 }
 
+// Uncomment for solution 1
+
+// class MinHeap {
+//   constructor() {
+//     this.values = [];
+//   }
+//   add(val) {
+//     this.values.push(val);
+//     let idx = this.values.length - 1;
+//     let parentIdx = Math.floor((idx - 1) / 2);
+//     while (parentIdx >= 0 && this.values[idx][2] < this.values[parentIdx][2]) {
+//       [this.values[parentIdx], this.values[idx]] = [this.values[idx], this.values[parentIdx]];
+//       idx = parentIdx;
+//       parentIdx = Math.floor((idx - 1) / 2);
+//     }
+//     return val;
+//   }
+//   remove() {
+//     if (!this.values.length) return -1;
+//     if (this.values.length === 1) return this.values.pop();
+//     let value = this.values[0];
+//     let popped = this.values.pop();
+//     this.values[0] = popped;
+//     let idx = 0;
+//     let leftIdx = idx * 2 + 1, rightIdx = idx * 2 + 2;
+//     let childIdx = getChild(this.values, leftIdx, rightIdx);
+//     function getChild(vals, leftIdx, rightIdx) {
+//       let end = vals.length - 1;
+//       if (leftIdx > end && rightIdx > end) return -1;
+//       if (rightIdx > end) return leftIdx;
+//       if (vals[leftIdx][2] < vals[rightIdx][2]) return leftIdx;
+//       return rightIdx;
+//     }
+//     while (childIdx > -1 && this.values[idx][2] > this.values[childIdx][2]) {
+//       [this.values[idx], this.values[childIdx]] = [this.values[childIdx], this.values[idx]];
+//       idx = childIdx;
+//       leftIdx = idx * 2 + 1, rightIdx = idx * 2 + 2;
+//       childIdx = getChild(this.values, leftIdx, rightIdx);
+//     }
+//     return value;
+//   }
+//   isEmpty() {
+//     return this.values.length === 0;
+//   }
+// }
+
+// <---------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+// Solution 2: Prim's Algorithm
+
+// Create an adjacency list to store all edges,
+  // the virtual edges (0 to i+1th well in wells)
+  // bidirectional edges for pipes ([house1, house2, cost] => house1 -> [house2, cost], house2 -> [house1, cost])
+// for each virtual edge, add [i + 1 (house), wells[i] (cost)] to a heap.
+// keep a 'visited' hashmap to keep track of houses we have been to.
+// mark 0 as visited.
+// Keep totalCost and count (number of edges should be n - 1 for a MST)
+// Loop while heap is not empty AND count is smaller than n
+  // let [house, cost] be heap.remove()
+  // if house has not been visited yet,
+    // increment totalCost by cost
+    // increment count by one
+    // mark house as visited
+    // loop through each [neighbor, nextCost] in graph[house]
+      // if neighbor has not been visited before,
+        // add [neighbor, nextCost] to the heap
+// Return totalCost
+
+// v = num of houses, e = pipes.length
+// Time Complexity: O(v + e * log(v + e)) 356ms
+// Space Complexity: O(v + e) 76.6MB
+var minCostToSupplyWater = function(n, wells, pipes) {
+  let heap = new MinHeap();
+  let graph = {};
+  for (var i = 0; i < n + 1; i++) {
+    graph[i] = [];
+  }
+  for (i = 0; i < wells.length; i++) {
+    graph[0].push([i + 1, wells[i]]);
+    heap.add([i + 1, wells[i]]);
+  }
+  for (var [house1, house2, cost] of pipes) {
+    graph[house1].push([house2, cost]);
+    graph[house2].push([house1, cost]);
+  }
+  let visited = {};
+  visited[0] = true;
+  let totalCost = 0, count = 0;
+  while (!heap.isEmpty() && count < n) {
+    let [house, cost] = heap.remove();
+    if (!visited[house]) {
+      totalCost += cost;
+      count++;
+      visited[house] = true;
+      for (var [neighbor, nextCost] of graph[house]) {
+        if (!visited[neighbor]) {
+          heap.add([neighbor, nextCost]);
+        }
+      }
+    }
+  }
+  return totalCost;
+};
+
+// Min heap for solution 2
 class MinHeap {
   constructor() {
     this.values = [];
@@ -87,7 +193,7 @@ class MinHeap {
     this.values.push(val);
     let idx = this.values.length - 1;
     let parentIdx = Math.floor((idx - 1) / 2);
-    while (parentIdx >= 0 && this.values[idx][2] < this.values[parentIdx][2]) {
+    while (parentIdx >= 0 && this.values[idx][1] < this.values[parentIdx][1]) {
       [this.values[parentIdx], this.values[idx]] = [this.values[idx], this.values[parentIdx]];
       idx = parentIdx;
       parentIdx = Math.floor((idx - 1) / 2);
@@ -107,10 +213,10 @@ class MinHeap {
       let end = vals.length - 1;
       if (leftIdx > end && rightIdx > end) return -1;
       if (rightIdx > end) return leftIdx;
-      if (vals[leftIdx][2] < vals[rightIdx][2]) return leftIdx;
+      if (vals[leftIdx][1] < vals[rightIdx][1]) return leftIdx;
       return rightIdx;
     }
-    while (childIdx > -1 && this.values[idx][2] > this.values[childIdx][2]) {
+    while (childIdx > -1 && this.values[idx][1] > this.values[childIdx][1]) {
       [this.values[idx], this.values[childIdx]] = [this.values[childIdx], this.values[idx]];
       idx = childIdx;
       leftIdx = idx * 2 + 1, rightIdx = idx * 2 + 2;
