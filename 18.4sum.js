fadf98d2bfceafff961225eaf7c31f02272a5cbf
Anna An <annaan.golf@gmail.com>
2021-10-06 16:19:47 +1300

    solution added

--- 18.4sum.js
+++ 18.4sum.js
@@ -54,10 +54,74 @@ var fourSum = function(nums, target) {
     kSum(4, 0, target, []);
     return result;
   };
+
+// Solution 2: K-Sum w/ Backtracking
+// let res be the array in which we store our quadruplets
+// First, sort nums in asc order.
+// Call kSum([], 0, 4, target)
+// return res
+
+// kSum: (arr, start index, k, target)
+  // if k is 2,
+    // call twoSum(arr, start, target)
+    // return
+  // otherwise, loop through from start (pointer = i)
+    // if i is not a duplicate,
+      // push nums[i] into arr
+      // call kSum(arr, i + 1, k - 1, target - nums[i])
+      // pop nums[i] out of arr (backtrack)
+
+// twoSum: (arr, start index, target)
+  // set two pointers, i to start and j to nums.length - 1
+  // loop while i is smaller than j
+    // let sum be nums[i] + nums[j]
+    // if sum is equal to target (found an answer!)
+      // make a deep copy of arr and push it together with nums[i] and nums[j] into res
+      // increment i and decrement j
+      // increment i while nums[i] is a duplicate (equal to the number before it, we only need it once)
+    // otherwise if sum is smaller than target, increment i by one
+    // otherwise if sum is bigger than target, decrement j by one
+
+// Time Complexity: O(n^(k-1) + n log(n)) 148ms
+// Space Complexity: O(n) 41MB
+var fourSum = function(nums, target) {
+  let res = [];
+  nums.sort((a, b) => a - b);
+  kSum([], 0, 4, target);
+  return res;
+
+  function kSum(arr, start, k, target) {
+    if (k === 2) {
+      twoSum(arr, start, target);
+      return;
+    }
+    for (var i = start; i < nums.length; i++) {
+      if (i === start || nums[i] !== nums[i - 1]) {
+        arr.push(nums[i]);
+        kSum(arr, i + 1, k - 1, target - nums[i]);
+        arr.pop();
+      }
+    }
+  }
+
+  function twoSum(arr, start, target) {
+    let i = start, j = nums.length - 1;
+    while (i < j) {
+      let sum = nums[i] + nums[j];
+      if (sum === target) {
+        res.push([...arr, nums[i], nums[j]]);
+        i++, j--;
+        while (nums[i] === nums[i - 1]) i++;
+      }
+      else if (sum < target) i++;
+      else j--;
+    }
+  }
+};
   
-  // Five test cases to run function on
-  console.log(fourSum([-3,-1,0,2,4,5],0)) // [[-3,-1,0,4]]
-  console.log(fourSum([0,0,0,0],0)) // [[0,0,0,0]]
-  console.log(fourSum([2,2,2,2,2,2], 8)) // [[2,2,2,2]] 
-  console.log(fourSum([1,0,-1,0,-2,2], 0)) // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
-  console.log(fourSum([2,2,2,2,2], 8)) // [[2,2,2,2]]
+// Five test cases to run function on
+console.log(fourSum([-3,-1,0,2,4,5],0)) // [[-3,-1,0,4]]
+console.log(fourSum([0,0,0,0],0)) // [[0,0,0,0]]
+console.log(fourSum([2,2,2,2,2,2], 8)) // [[2,2,2,2]] 
+console.log(fourSum([1,0,-1,0,-2,2], 0)) // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+console.log(fourSum([2,2,2,2,2], 8)) // [[2,2,2,2]]
