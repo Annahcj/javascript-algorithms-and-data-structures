b140ace1a946eaecadfe1e221c1c9071f8edf67f
Anna An <annaan.golf@gmail.com>
2021-10-11 17:24:33 +1300

    solution added

--- 543.diameter-of-binary-tree.js
+++ 543.diameter-of-binary-tree.js
@@ -3,6 +3,7 @@
 // The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
 // The length of a path between two nodes is represented by the number of edges between them.
 
+
 // LeetCode provided TreeNode
 function TreeNode(val, left, right) {
   this.val = (val===undefined ? 0 : val)
