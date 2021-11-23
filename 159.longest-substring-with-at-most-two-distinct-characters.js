1bbe4566af7d2f5e9d994ed350c2bcbeed604302
Anna An <annaan.golf@gmail.com>
2021-11-24 12:27:14 +1300

    solution added

--- 159.longest-substring-with-at-most-two-distinct-characters.js
+++ 159.longest-substring-with-at-most-two-distinct-characters.js
@@ -2,48 +2,42 @@
 // Given a string s, return the length of the longest substring that contains at most two distinct characters.
 
 
-// Solution: Sliding Window 
+// Solution: Sliding Window
 
-// Use a hashmap to keep the last occurance indexes
-// set two variables char1 and char2
-// set max to 0, start to 0
-// loop through s (pointer = i)
-  // set the lastIdx[s[i]] to i
-  // if s[i] is not char1 and not char2, (then we need to move our window)
-    // set max to Math.max(max, i - start)
-    // if s[i - 1] is equal to fruit1
-      // set start to the last index of char2 + 1 (that is the first valid index of char2)
-      // delete lastIdx[char2] to save space
-      // set char2 to fruit
-    // otherwise 
-      // set start to the last index of char1 + 1
-      // delete lastIdx[char1] to save space
-      // set char1 to fruit
-  // set max to Math.max(max, i - start)
-  // return max
+// Use two pointers -> left and right
+// Keep a hashmap 'freq' which stores the number of times a character has occured.
 
-// Time Complexity: O(n) 236ms
-// Space Complexity: O(1) 45.8MB
+// if the frequency of s[right] is 0, set the frequency to 1, and decrement unique by 1.
+// otherwise, increment the frequency of s[right].
+// increment right.
+// while unique is smaller than 0,
+  // decrement the frequency of s[left] by 1
+  // if the frequency of s[left] is 0, increment unique by 1.
+  // increment left.
+// keep track of the max length
+
+// Time Complexity: O(n) 116ms
+// Space Complexity: O(n) 41.5MB
 var lengthOfLongestSubstringTwoDistinct = function(s) {
-  let lastIdx = {};
-  let char1, char2;
-  let max = 0, start = 0;
-  for (var i = 0; i < s.length; i++) {
-    lastIdx[s[i]] = i;
-    if (s[i] !== char1 && s[i] !== char2) {
-      max = Math.max(max, i - start);
-      if (i > 0 && s[i - 1] === char1) {
-        start = lastIdx[char2] !== undefined ? lastIdx[char2] + 1 : 0;
-        delete lastIdx[char2];
-        char2 = s[i];
-      } else {
-        start = lastIdx[char1] !== undefined ? lastIdx[char1] + 1 : 0;
-        delete lastIdx[char1];
-        char1 = s[i];
-      }
+  let freq = {};
+  let left = 0, right = 0;
+  let unique = 2;
+  let max = 0;
+  while (right < s.length) {
+    if (!freq[s[right]]) {
+      freq[s[right]] = 1;
+      unique--;
+    } else {
+      freq[s[right]]++;
     }
-  }  
-  max = Math.max(max, i - start);
+    right++;
+    while (unique < 0) {
+      freq[s[left]]--;
+      if (freq[s[left]] === 0) unique++;
+      left++;
+    }
+    max = Math.max(max, right - left);
+  }
   return max;
 };
 
