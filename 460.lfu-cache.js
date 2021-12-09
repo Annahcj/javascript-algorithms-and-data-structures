7f8739df0709a71e1662eb986e5db2688feb79f5
Anna An <annaan.golf@gmail.com>
2021-12-10 12:51:07 +1300

    solution added

--- 460.lfu-cache.js
+++ 460.lfu-cache.js
@@ -20,135 +20,160 @@
 // LFUCache:
 // hashmap for the frequencies - freqMap
 // hashmap for the nodes/vals - nodeMap
-// leastFreq - the frequency of the least frequently used item
+// minFreq - the frequency of the least frequently used item
 // capacity, which will be passed in upon creation
 // size, which will be incremented and decremented as items are added and removed.
 
 // get: (accepts a key)
-// If there is no such node in nodeMap, return -1;
-// Otherwise, remove the node from the freqMap
-// If leastFreq is equal to node.freq AND the linked list in freqMap[node.freq] is empty, increment leastFreq
-// Increment node.freq too.
-// If freqMap[node.freq] (the new frequency of the node) doesn't exist yet, set it to a new doubly linked list.
-// Insert the node at the head of the doubly linked list (freqMap[node.freq])
-// Return node.val.
+  // If node doesn't exist, return -1.
+  // 1. Get the node from the nodeMap
+  // 2. Get the frequency of the node: oldFreq
+  // 3. Get the linked list for oldFreq: list
+  // 4. Remove the node from the list
+  // 5. Increment the node's frequency by 1: newFreq
+  // 6. If the list is now empty AND minFreq is equal to oldFreq, increment minFreq by 1.
+  // 7. If there is no for the newFreq yet, create a new linked list.
+  // 8. Add the node to the head of the new list.
+  // 9. Return the value of the node.
 
 // put: (accepts a key and value) 
-// If capacity is zero, return. (no items allowed at all)
-// If node doesn't exist in nodeMap, 
-  // Make a new Node.
-  // Increment size of cache by one.
-  // If the size is bigger than the capacity
-    // remove the last node in dll of freqMap[leastFreq]
-    // removeTail returns the key of removed tail node, delete node in nodeMap with key of old tail node
-    // delete the node from the nodeMap
-    // decrement size by one
-  // add newNode to nodeMap
-  // add newNode to freqMap, update leastFreq if necessary (since its a new node, frequency will always be one, and one is the least possible frequency so we check if freqMap has frequency of 1)
-// Else if node exists in nodeMap,
-  // Update the value of the node
-  // Now we have to update the frequency
-  // Remove the node from the linked list in freqMap[node.freq]
-  // If node's freq is equal to least freq AND node was the only one with that frequency, increment least frequency.
-  // Increment node's frequency
-  // If freqMap[node.freq] doesn't exist, assign it a new doubly linked list.
-  // Insert the node at the head of the list of the new frequency in freqMap.
+  // If the nodeMap contains the key
+    // 1. Get the node from the nodeMap
+    // 2. Update the value of the node
+    // 3. Get the current frequency: oldFreq
+    // 4. Get the current linked list: list
+    // 5. Remove the node from list
+    // 6. If the list is now empty AND the minFreq is equal to oldFreq, increment minFreq by 1.
+    // 7. Increment the node's frequency by 1: newFreq
+    // 8. If there is no list for newFreq yet, create a new linked list.
+    // 9. Add the node to the head of the new list.
+  // Otherwise,
+    // If the capacity is 0, return.
+    // 1. Create a new node: newNode
+    // 2. If there is no list for the frequency 1 yet, create a new list: list
+    // 3. If the size is equal to the capacity, remove the tail from the list with the minFreq. Decrement size by 1.
+    // 4. Set minFreq to 1 
+    // 5. Increment the size by 1.
+    // 6. Add newNode to the head of list.
+    // 7. Save the node in the nodeMap
+
   
-// Runtime on LeetCode: 716ms
-// Memory Usage on LeetCode: 93.1MB
+// Runtime on LeetCode: 692ms
+// Memory Usage on LeetCode: 91.9MB
 // Time Complexity for all methods: O(1)
 
-  class Node {
-    constructor(key, val) {
-      this.key = key;
-      this.val = val;
-      this.next = null;
-      this.prev = null;
-      this.freq = 1;
-    }
-  }
-  class DoublyLinkedList {
-    constructor() {
-      this.head = new Node(null, null);
-      this.tail = new Node(null, null);
-      this.head.next = this.tail;
-      this.tail.prev = this.head;
-    }
-    insertHead(node) {
-      node.next = this.head.next;
-      node.prev = this.head;
-      this.head.next.prev = node;
-      this.head.next = node;
-    }
-    removeNode(node) {
-      let prev = node.prev;
-      let next = node.next;
-      prev.next = next;
-      next.prev = prev;
-    }
-    removeTail() {
-      let tail = this.tail.prev;
-      this.removeNode(tail);
-      return tail.key;
-    }
-    isEmpty() {
-      return this.head.next.val === null;
-    }
-  }
-  var LFUCache = function(capacity) {
-    this.nodeMap = {};
-    this.freqMap = {};
-    this.leastFreq = 0;
-    this.capacity = capacity;
-    this.size = 0;
-  };
-  
-  LFUCache.prototype.get = function(key) {
+var LFUCache = function(capacity) {
+  this.nodeMap = {};
+  this.freqMap = {};
+  this.capacity = capacity;
+  this.minFreq = 1;
+  this.size = 0;
+};
+
+LFUCache.prototype.get = function(key) {
+  if (!this.nodeMap[key]) return -1;
+  let node = this.nodeMap[key];
+  let oldFreq = node.freq;
+  let list = this.freqMap[oldFreq];
+  list.removeNode(node);
+  node.freq++;
+  let newFreq = node.freq;
+  if (list.isEmpty() && this.minFreq === oldFreq) this.minFreq++;
+  if (!this.freqMap[newFreq]) this.freqMap[newFreq] = new DoublyLinkedList();
+  this.freqMap[newFreq].insertHead(node);
+  return node.val;
+};
+
+LFUCache.prototype.put = function(key, value) {
+  if (this.nodeMap[key]) {
     let node = this.nodeMap[key];
-    if (!node) return -1;
-    this.freqMap[node.freq].removeNode(node);
-    if (node.freq === this.leastFreq && this.freqMap[node.freq].isEmpty()) this.leastFreq++;
+    node.val = value;
+    let oldFreq = node.freq;
+    let list = this.freqMap[oldFreq];
+    list.removeNode(node);
+    if (list.isEmpty() && this.minFreq === oldFreq) this.minFreq++;
     node.freq++;
-    if (!this.freqMap[node.freq]) this.freqMap[node.freq] = new DoublyLinkedList();
-    this.freqMap[node.freq].insertHead(node);
-    return node.val;
-  };
-  
-  LFUCache.prototype.put = function(key, value) {
-    if (!this.capacity) return;
-    let node = this.nodeMap[key];
-    if (!node) {
-      let newNode = new Node(key, value);
-      this.size++;
-      if (this.size > this.capacity) {
-        let tailKey = this.freqMap[this.leastFreq].removeTail();
-        delete this.nodeMap[tailKey];
-        this.size--;
-      }
-      this.nodeMap[key] = newNode;
-      if (!this.freqMap[1]) this.freqMap[1] = new DoublyLinkedList();
-      this.freqMap[1].insertHead(newNode);
-      this.leastFreq = 1;
-    } else {
-      node.val = value;
-      this.freqMap[node.freq].removeNode(node);
-      if (node.freq === this.leastFreq && this.freqMap[node.freq].isEmpty()) this.leastFreq++;
-      node.freq++;
-      if (!this.freqMap[node.freq]) this.freqMap[node.freq] = new DoublyLinkedList();
-      this.freqMap[node.freq].insertHead(node);
+    let newFreq = node.freq;
+    if (!this.freqMap[newFreq]) this.freqMap[newFreq] = new DoublyLinkedList();
+    this.freqMap[newFreq].insertHead(node);
+  } else {
+    if (this.capacity === 0) return;
+    let newNode = new Node(key, value);
+    if (!this.freqMap[1]) this.freqMap[1] = new DoublyLinkedList();
+    let list = this.freqMap[1];
+    if (this.size === this.capacity) {
+      let removedKey = this.freqMap[this.minFreq].removeTail();
+      delete this.nodeMap[removedKey];
+      this.size--;
     }
-  };
+    this.minFreq = 1;
+    this.size++;
+    list.insertHead(newNode);
+    this.nodeMap[key] = newNode;
+  }
+};
+
+class Node {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+    this.freq = 1;
+  }
+}
+// dummy head and tail
+class DoublyLinkedList {
+  constructor() {
+    this.head = new Node(null, null);
+    this.tail = new Node(null, null);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+  insertHead(node) {
+    node.next = this.head.next;
+    node.prev = this.head;
+    this.head.next.prev = node;
+    this.head.next = node;
+  }
+  insertTail(node) {
+    node.prev = this.tail.prev;
+    node.next = this.tail;
+    this.tail.prev.next = node;
+    this.tail.prev = node;
+  }
+  removeHead() {
+    let head = this.head.next;
+    this.removeNode(head);
+    return head.val;
+  }
+  removeNode(node) {
+    if (!node.prev && !node.next) return;
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    node.prev = null;
+    node.next = null;
+  }
+  removeTail() {
+    let tail = this.tail.prev;
+    this.removeNode(tail);
+    return tail.key;
+  }
+  isEmpty() {
+    return this.head.next.val === null;
+  }
+}
   
-  // A few test cases 
-  let cache = new LFUCache(2)
-  console.log(cache.put(1,1)) // null
-  console.log(cache.put(2,2)) // null
-  console.log(cache.get(1)) // 1
-  console.log(cache.put(3,3)) // null
-  console.log(cache.get(2)) // -1
-  console.log(cache.get(3)) // 3
-  console.log(cache.put(4,4)) // null
-  console.log(cache.get(1)) // -1
-  console.log(cache.get(3)) // 3
-  console.log(cache.get(4)) // 4
-  console.log(cache)
+// A few test cases 
+let cache = new LFUCache(2)
+console.log(cache.put(1,1)) // null
+console.log(cache.put(2,2)) // null
+console.log(cache.get(1)) // 1
+console.log(cache.put(3,3)) // null
+console.log(cache.get(2)) // -1
+console.log(cache.get(3)) // 3
+console.log(cache.put(4,4)) // null
+console.log(cache.get(1)) // -1
+console.log(cache.get(3)) // 3
+console.log(cache.get(4)) // 4
+console.log(cache)
