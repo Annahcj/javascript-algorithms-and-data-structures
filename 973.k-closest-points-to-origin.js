8f416f5a119b492e63f1b7c7cc6fad7ea8d6abda
Anna An <annaan.golf@gmail.com>
2022-01-11 23:11:24 +1300

    solution added

--- 973.k-closest-points-to-origin.js
+++ 973.k-closest-points-to-origin.js
@@ -1,81 +1,76 @@
 // 973. K Closest Points to Origin
 // Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+// The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+// You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
 
 
-// Solution 1: Priority Queue / Min Heap
+// Solution: Heap
 
-// Notes:
-// Since the origin is always (0,0), we can simply calculate x^2 + y^2 for each point, then find the coordinates with the lowest.
-// Square rooting the distance is also unneccessary.
+// Order the heap by the biggest distance first.
+// When the size of the heap exceeds k, remove the max item from it.
+// When everything is added, the heap will consist of the k closest points.
 
-// Algorithm:
-// Initialize a min heap. The heap will be ordered by the distance of each point.
-// Loop through points, 
-  // add the distance and x-y coordinates to the heap
-// - end of iteration -
-// Loop k number of times,
-  // push the coordinates of 'heap.remove().coordinates' into the result array (heap.remove() returns the first item before removal)
-// - end of iteration -
-// Return result
+// Time Complexity: O(n log(k)) 314ms
+// Space Complexity: O(k) 71.5MB
+var kClosest = function(points, k) {
+  let heap = new PriorityQueue((a, b) => b.distance - a.distance);
+  for (var i = 0; i < points.length; i++) {
+    let distance = (points[i][0] ** 2) + (points[i][1] ** 2);
+    heap.add({distance, coordinates: points[i]});
+    if (heap.size > k) heap.remove();
+  }
+  let res = [];
+  for (var val of heap.values) {
+    res.push(val.coordinates);
+  }
+  return res;
+};
 
-// Time Complexity: O(n log(n)) 200ms
-// Space Complexity: O(n) 55.9MB
-  var kClosest = function(points, k) {
-    let heap = new MinHeap();
-    let res = [];
-    for (var i = 0; i < points.length; i++) {
-      let distance = (points[i][0] ** 2) + (points[i][1] ** 2);
-      heap.add({distance, coordinates: points[i]});
+class PriorityQueue {
+  constructor(comparator = ((a, b) => a - b)) {
+    this.values = [];
+    this.comparator = comparator;
+    this.size = 0;
+  }
+  add(val) {
+    this.size++;
+    this.values.push(val);
+    let idx = this.size - 1, parentIdx = Math.floor((idx - 1) / 2);
+    while (parentIdx >= 0 && this.comparator(this.values[parentIdx], this.values[idx]) > 0) {
+      [this.values[parentIdx], this.values[idx]] = [this.values[idx], this.values[parentIdx]];
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
     }
-    for (var j = 0; j < k; j++) {
-      res.push(heap.remove().coordinates);
-    }
-    return res;
-  };
-  
-  // Min Heap, ordered by the distance to origin.
-    class MinHeap {
-      constructor() {
-        this.values = [];
-      }
-      add(value) {
-        let pushed = this.values.push(value);
-        let currIdx = this.values.length - 1;
-        let parentIdx = Math.floor((currIdx - 1) / 2);
-        while (parentIdx >= 0 && this.values[parentIdx].distance > this.values[currIdx].distance) {
-          [this.values[parentIdx], this.values[currIdx]] = [this.values[currIdx], this.values[parentIdx]];
-          currIdx = parentIdx;
-          parentIdx = Math.floor((currIdx - 1) / 2);
+  }
+  remove() {
+    if (this.size === 0) return -1;
+    this.size--;
+    if (this.size === 0) return this.values.pop();
+    let removedVal = this.values[0];
+    this.values[0] = this.values.pop();
+    let idx = 0;
+    while (idx < this.size && idx < Math.floor(this.size / 2)) {
+      let leftIdx = idx * 2 + 1, rightIdx = idx * 2 + 2;
+      if (rightIdx === this.size) {
+        if (this.comparator(this.values[leftIdx], this.values[idx]) > 0) break;
+        [this.values[leftIdx], this.values[idx]] = [this.values[idx], this.values[leftIdx]];
+        idx = leftIdx;
+      } else if (this.comparator(this.values[leftIdx], this.values[idx]) < 0 || this.comparator(this.values[rightIdx], this.values[idx]) < 0) {
+        if (this.comparator(this.values[leftIdx], this.values[rightIdx]) <= 0) {
+          [this.values[leftIdx], this.values[idx]] = [this.values[idx], this.values[leftIdx]];
+          idx = leftIdx;
+        } else {
+          [this.values[rightIdx], this.values[idx]] = [this.values[idx], this.values[rightIdx]];
+          idx = rightIdx;
         }
-        return pushed;
-      }
-      remove() {
-        if (!this.values.length) return -1;
-        let currIdx = 0, removedVal = this.values[0];
-        [this.values[0], this.values[this.values.length - 1]] = [this.values[this.values.length - 1], this.values[0]];
-        this.values.pop();
-        let leftIdx = currIdx * 2 + 1, rightIdx = currIdx * 2 + 2;
-        function getSmaller(vals, leftIdx, rightIdx) {
-          if (rightIdx < vals.length) {
-            if (vals[leftIdx].distance < vals[rightIdx].distance) {
-              return leftIdx;
-            } return rightIdx;
-          } else {
-            if (leftIdx < vals.length) return leftIdx;
-            return -1;
-          }
-        }
-        let smallerChild = getSmaller(this.values, leftIdx, rightIdx);
-        while (smallerChild > -1 && this.values[smallerChild].distance < this.values[currIdx].distance) {
-          [this.values[currIdx], this.values[smallerChild]] = [this.values[smallerChild], this.values[currIdx]];
-          currIdx = smallerChild;
-          leftIdx = currIdx * 2 + 1, rightIdx = currIdx * 2 + 2;
-          smallerChild = getSmaller(this.values, leftIdx, rightIdx);
-        }
-        return removedVal;
+      } else {
+        break;
       }
     }
-  
-  // Two test cases to run function on
-  console.log(kClosest([[1,3],[-2,2]], 1)) // [[-2,2]]
-  console.log(kClosest([[3,3],[5,-1],[-2,4]], 2)) // [[3,3],[-2,4]]
+    return removedVal;
+  }
+}
+
+// Two test cases to run function on
+console.log(kClosest([[1,3],[-2,2]], 1)) // [[-2,2]]
+console.log(kClosest([[3,3],[5,-1],[-2,4]], 2)) // [[3,3],[-2,4]]
