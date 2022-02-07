920b7b29a870789e55fdb65458953b1ad6b47a0f
Anna An <annaan.golf@gmail.com>
2022-02-08 11:40:32 +1300

    Add solution

--- 2007.find-original-array-from-doubled-array.js
+++ 2007.find-original-array-from-doubled-array.js
@@ -25,11 +25,11 @@ var findOriginalArray = function(changed) {
   
   let res = [];
   for (let num of changed) {
-    if (freq.get(num) === 0) continue;
-    freq.set(num, freq.get(num) - 1);
-    if (!freq.has(num * 2) || freq.get(num * 2) === 0) return [];
-    freq.set(num * 2, freq.get(num * 2) - 1);
-    res.push(num);
+    if (freq.get(num) === 0) continue; // number is already used up
+    freq.set(num, freq.get(num) - 1); // decrease num frequency
+    if (!freq.has(num * 2) || freq.get(num * 2) === 0) return []; // there is no double, invalid.
+    freq.set(num * 2, freq.get(num * 2) - 1); // decrease double frequency
+    res.push(num); 
   }
   return res;
 };
