e372012d7359395b3db13bc335f455684b999612
Anna An <annaan.golf@gmail.com>
2022-04-12 22:52:28 +1200

    Add solution

--- 705.design-hashset.js
+++ 705.design-hashset.js
@@ -6,7 +6,7 @@
   // void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
 
 
-// Solution: Array & Linked List (separate chaining)
+// Solution 1: Array & Linked List (separate chaining)
 
 // Use an array with a set size (a prime number like 937)
 // Have a linked list for each bucket for separate chaining (when a multiple keys have the same hash value, chain them using a linked list)
@@ -48,7 +48,7 @@ MyHashSet.prototype.getHash = function(num) {
 
 // Singly Linked List
 
-class Node {
+class ListNode {
   constructor(val) {
     this.val = val;
     this.next = null;
@@ -57,12 +57,12 @@ class Node {
 
 class LinkedList {
   constructor() {
-    this.head = new Node(null);
+    this.head = new ListNode(null);
     this.tail = this.head;
     this.size = 0;
   }
   add(val) {
-    let node = new Node(val);
+    let node = new ListNode(val);
     this.tail.next = node;
     this.tail = node;
     this.size++;
@@ -96,6 +96,128 @@ class LinkedList {
       node = node.next;
     }
     return false;
+  }
+}
+
+
+// Solution 2: Array & Binary Search Tree
+
+// Instead of using a linked list for each bucket, use a binary search tree.
+// This brings the time complexity for add, remove, and contains to O(log(n / k)) on average.
+
+// Time Complexity for add, remove, contains: O(log(n / k)) 208ms
+// Space Complexity: O(k + m) 57.2MB
+var MyHashSet = function() {
+  this.range = 937;
+  this.hashset = Array(this.range);
+  for (let i = 0; i < this.range; i++) {
+    this.hashset[i] = new BST();
+  }
+};
+
+MyHashSet.prototype.add = function(key) {
+  let hashKey = this.getHash(key);  
+  let bucket = this.hashset[hashKey];
+  if (!bucket.contains(key)) bucket.insert(key);
+};
+
+MyHashSet.prototype.remove = function(key) {
+  let hashKey = this.getHash(key);  
+  this.hashset[hashKey].remove(key);
+};
+
+MyHashSet.prototype.contains = function(key) {
+  let hashKey = this.getHash(key);  
+  return this.hashset[hashKey].contains(key);
+};
+
+MyHashSet.prototype.getHash = function(num) {
+  return (num * 7907) % this.range;
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let node = this.root;
+      while (true) {
+        if (node.val < val) {
+          if (!node.right) {
+            node.right = newNode;
+            break;
+          } else {
+            node = node.right;
+          }
+        } else {
+          if (!node.left) {
+            node.left = newNode;
+            break;
+          } else {
+            node = node.left;
+          }
+        }
+      }
+    }
+  }
+  contains(val) {
+    let node = this.root;
+    while (node) {
+      if (node.val === val) return true;
+      else if (node.val < val) node = node.right;
+      else node = node.left;
+    }
+    return false;
+  }
+  remove(val) {
+    let node = this.root;
+    if (!node) return false;
+    if (node.val === val) {
+      this.root = replace(node);
+      return true;
+    }
+
+    while (node) {
+      if (node.left && node.left.val === val) {
+        node.left = replace(node.left);
+        return true;
+      } else if (node.right && node.right.val === val) {
+        node.right = replace(node.right);
+        return true;
+      }
+
+      if (node.val < val) node = node.right;
+      else node = node.left;
+    }
+
+    function replace(node) {
+      if (!node.left && !node.right) return null;
+      if (!node.left) return node.right;
+      if (!node.right) return node.left;
+      
+      let rightNode = findRight(node.left);
+      rightNode.right = node.right;
+      return node.left;
+    }
+
+    function findRight(node) {
+      while (node.right) {
+        node = node.right;
+      }
+      return node;
+    }
   }
 }
 
