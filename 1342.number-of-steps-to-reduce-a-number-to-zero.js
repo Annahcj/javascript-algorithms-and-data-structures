600e198067120b9588ad5130ce0bce1167dc72f9
Anna An <annaan.golf@gmail.com>
2022-05-27 16:16:52 +1200

    Add solution

---
+++ 1342.number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,23 @@
+// 1342. Number of Steps to Reduce a Number to Zero
+// Given an integer num, return the number of steps to reduce it to zero.
+// In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+
+// Solution: Simulation
+
+// Time Complexity: O(log(n)) 67ms
+// Space Complexity: O(1) 42.3MB
+var numberOfSteps = function(num) {
+  let steps = 0;
+  while (num > 0) {
+    if (num % 2 === 1) num--;
+    else num /= 2;
+    steps++;
+  }  
+  return steps;
+};
+
+// Three test cases to run function on
+console.log(numberOfSteps(14)) // 6
+console.log(numberOfSteps(8)) // 4
+console.log(numberOfSteps(123)) // 12
