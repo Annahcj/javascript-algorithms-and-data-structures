466058302d902ac6fe2e59729c7b826f52d776e7
Anna An <annaan.golf@gmail.com>
2022-06-17 21:33:50 +1200

    Add solution

---
+++ 968.binary-tree-cameras.js
@@ -0,0 +1,28 @@
+// 968. Binary Tree Cameras
+// You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a node can monitor its parent, itself, and its immediate children.
+// Return the minimum number of cameras needed to monitor all nodes of the tree.
+
+
+// Solution: Recursive DFS
+
+// There are three situations to take into account:
+  // 1. Node is null, return CAMERA_NOT_NEEDED (we don't need a camera for the parent node)
+  // 2. The left or right child does not have a camera: add a camera to the current node and increase the count.
+  // 3. If the left or right child has a camera, then we don't need a camera. Otherwise we return NOT_COVERED for the parent nodes.
+
+// Time Complexity: O(n) 103ms
+// Space Complexity: O(h) 45.9MB
+var minCameraCover = function(root) {
+  let NOT_COVERED = 0, HAS_CAMERA = 1, CAMERA_NOT_NEEDED = -1, res = 0;
+  return dfs(root) === 0 ? res + 1 : res;
+  
+  function dfs(node) {
+    if (!node) return CAMERA_NOT_NEEDED;
+    let left = dfs(node.left), right = dfs(node.right);
+    if (left === NOT_COVERED || right === NOT_COVERED) {
+      res++;
+      return HAS_CAMERA;
+    }
+    return left === HAS_CAMERA || right === HAS_CAMERA ? CAMERA_NOT_NEEDED : NOT_COVERED;
+  }  
+};
