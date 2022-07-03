7da7a711b357f2c31b575fa120310825b3670d94
Anna An <annaan.golf@gmail.com>
2022-07-03 17:28:42 +1200

    Add solution

--- 2327.number-of-people-aware-of-a-secret.js
+++ 2327.number-of-people-aware-of-a-secret.js
@@ -1,7 +1,7 @@
 // 2327. Number of People Aware of a Secret
 // On day 1, one person discovers a secret.
 // You are given an integer delay, which means that each person will share the secret with a new person every day, starting from delay days after discovering the secret. You are also given an integer forget, which means that each person will forget the secret forget days after discovering it. A person cannot share the secret on the same day they forgot it, or on any day afterwards.
-// Given an integer n, return the number of people who know the secret at the end of day n. Since the answer may be very large, return it modulo 109 + 7.
+// Given an integer n, return the number of people who know the secret at the end of day n. Since the answer may be very large, return it modulo 10^9 + 7.
 
 
 // Solution: Recursion w/ Memoization
