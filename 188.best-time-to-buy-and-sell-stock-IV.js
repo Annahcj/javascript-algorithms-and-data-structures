0e6f52de580332394a52056132944364beea695e
Anna An <annaan.golf@gmail.com>
2022-09-10 15:31:53 +1200

    Add solution

--- 188.best-time-to-buy-and-sell-stock-IV.js
+++ 188.best-time-to-buy-and-sell-stock-IV.js
@@ -4,6 +4,8 @@
 // Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
 
 
+// Solution 1: DP - Tabulation
+
 // keep two arrays -> 
   // buy -> buy[i] denotes max profit with i + 1 transactions after buying 
   // sell -> sell[i] denotes max profit with i + 1 transactions after selling what was bought
@@ -32,6 +34,38 @@ var maxProfit = function(k, prices) {
     }
   }
   return sell[k - 1];
+};
+
+
+// Solution 2: DP - Recursion w/ Memoization
+
+// Memoize each dp(i, k, isBuy), where
+  // i = the ith day
+  // k = number of transactions we have leftover
+  // isBuy = whether the current transaction must be a buy 
+    // if isBuy = 1, the next action must be a buy.
+    // if isBuy = 0, the next action must be a sell.
+
+// For each dp(i, k, isBuy), we have three different choices:
+  // 1. Do nothing. We don't perform any action on the ith day.
+  // 2. If isBuy is 1, buy the stock on the ith day. (Note that we don't decrement k when we buy, only when we sell)
+  // 3. If isBuy is 0, sell the stock on the ith day. (Since the transaction is finished, we decrement k here)
+
+// Time Complexity: O(nk) 110ms
+// Space Complexity: O(nk) 57.3MB
+var maxProfit = function(k, prices) {
+  let n = prices.length, memo = Array(n).fill(0).map(() => Array(k + 1).fill(0).map(() => Array(2).fill(-1)));
+  return dp(0, k, 1);
+  
+  function dp(i, k, isBuy) {
+    if (i === n || k === 0) return 0;
+    if (memo[i][k][isBuy] !== -1) return memo[i][k][isBuy];
+    
+    let maxProfit = dp(i + 1, k, isBuy); // do nothing
+    if (isBuy) maxProfit = Math.max(maxProfit, dp(i + 1, k, 0) - prices[i]); // buy
+    else maxProfit = Math.max(maxProfit, dp(i + 1, k - 1, 1) + prices[i]); // sell
+    return memo[i][k][isBuy] = maxProfit;
+  }
 };
 
 // Two test cases to run function on
