a33cb1ac80f4d566cf3c44d2d919f9e6b5dd1e5d
Anna An <annaan.golf@gmail.com>
2022-10-26 22:24:27 +1300

    Add solution

--- 523.continuous-subarray-sum.js
+++ 523.continuous-subarray-sum.js
@@ -3,36 +3,28 @@
 // An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
 
 
-// Solution: Prefix Sum & Hashmap
+// Solution: Prefix Sum w/ Modulo
 
-// Pretend k is 6.
-// Let's say we have two prefix sums: 4,5,10
-// 10 - 4 = 6. 6 is a multiple of 6.
-// Let's consider another set of prefix sums: 1,5,25
-// 25 - 1 = 24. 24 is a multiple of 6. But how do we check for this?
-// One way is using mods:
+// Keep track of the previous modulo prefix sums.
+// For a valid subarray, there should be a previous modulo prefix sum with the same modulo as the current prefix sum. 
+  // This is because the sum of the subarray % k should be equal to 0.
 
-// Notice that 10 % 6 = 4, 4 % 6 = 4.
-// Also: 25 % 6 = 1, 1 % 6 = 1.
+// Since the subarray size must be at least two, we can try to find the longest subarray possible.
+// To get the longest subarray, we only need to keep track of the earliest index of a prefix sum.
 
-// The key here is that when two prefix sums modular k are equal, that means we have found a subarray with a sum that is a multiple of k.
-
-// Note: Since the subarray must have a length >= 2, AND we only have to return true or false, 
-// we can store the index of the FIRST prefix sum in mods[prefix sum % k].
-
-// mods[i] = index of first prefix sum where prefix sum % k = i.
-
-// Time Complexity: O(n)
-// Space Complexity: O(n)
+// Time Complexity: O(n) 104ms
+// Space Complexity: O(n) 64.3MB
 var checkSubarraySum = function(nums, k) {
-  let mods = Array(k);
-  mods[0] = -1;
-  let sum = 0;
-  for (var i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    let mod = sum % k;
-    if (mods[mod] !== undefined && mods[mod] !== i - 1) return true; // if we have a prefix sum with the same mod value AND the index is less than i - 1.
-    if (mods[mod] === undefined) mods[mod] = i;
+  let map = new Map(), n = nums.length, sum = 0;
+  map.set(0, -1);
+  for (let i = 0; i < n; i++) {
+    sum = (sum + nums[i]) % k;
+    if (map.has(sum)) {
+      let prevIndex = map.get(sum);
+      if (i - prevIndex >= 2) return true;
+    } else {
+      map.set(sum, i);
+    }
   }
   return false;
 };
