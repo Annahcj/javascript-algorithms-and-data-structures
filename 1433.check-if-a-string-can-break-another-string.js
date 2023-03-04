d52f05f95108d71c6baffaea13946c9fb4532392
Anna An <annaan.golf@gmail.com>
2023-03-05 12:21:11 +1300

    Add solution

--- 1433.check-if-a-string-can-break-another-string.js
+++ 1433.check-if-a-string-can-break-another-string.js
@@ -3,7 +3,7 @@
 // A string x can break string y (both of size n) if x[i] >= y[i] (in alphabetical order) for all i between 0 and n-1.
 
 
-// Solution: Greedy w/ Sorting
+// Solution 1: Greedy w/ Sorting
 
 // Sort both s1 and s2 in lexiographical order.
 // Check if either of the sorted s1 and s2 is consistently bigger/equal or smaller/equal than the other.
@@ -22,6 +22,33 @@ var checkIfCanBreak = function(s1, s2) {
       if (s1Bigger === -1) return false;
       s1Bigger = 1;
     } 
+  }
+  return true;
+};
+
+// Solution 2: Counting
+
+// Instead of sorting, we can use counting to bring the time complexity down to O(n) and space complexity to O(1).
+// Count the occurances of each character in s1 and s2.
+// Check whether the occurances of characters in one string always comes before the other.
+  // The balance is always negative/equal or positive/equal.
+
+// Time Complexity: O(n) 71ms
+// Space Complexity: O(1) 45.4MB
+var checkIfCanBreak = function(s1, s2) {
+  let count1 = Array(26).fill(0), count2 = Array(26).fill(0);
+  let n = s1.length;
+  for (let i = 0; i < n; i++) {
+    count1[s1.charCodeAt(i) - 97]++;
+    count2[s2.charCodeAt(i) - 97]++;
+  }
+  let currBalance = 0, balance = 0;
+  for (let i = 0; i < 26; i++) {
+    currBalance += count1[i] - count2[i];
+    if (currBalance !== 0) {
+      if ((balance > 0 && currBalance < 0) || (balance < 0 && currBalance > 0)) return false;
+      balance = currBalance;
+    }
   }
   return true;
 };
