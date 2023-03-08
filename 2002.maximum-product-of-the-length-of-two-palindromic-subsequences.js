9b9950effbb5a2cc96bee69e2cea020c37f78b1f
Anna An <annaan.golf@gmail.com>
2023-03-08 21:32:35 +1300

    Add solution

--- 2002.maximum-product-of-the-length-of-two-palindromic-subsequences.js
+++ 2002.maximum-product-of-the-length-of-two-palindromic-subsequences.js
@@ -4,7 +4,7 @@
 // A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters. A string is palindromic if it reads the same forward and backward.
 
 
-// Solution: Backtracking
+// Solution 1: Backtracking
 
 // For each s[i] we have three choices:
   // 1. Skip it.
@@ -36,6 +36,58 @@ var maxProduct = function(s) {
     let start = 0, end = s.length - 1;
     while (start < end) {
       if (s[start] !== s[end]) return false;
+      start++, end--;
+    }
+    return true;
+  }
+};
+
+
+// Solution 2: Generate Bitmasks
+
+// Generate all possible bitmasks up to 2^n.
+// Save the bitmasks that represent a palindrome.
+// Go through each pair of bitmasks, if the two bitmasks have no overlap, record the maximum product of lengths.
+
+// Time Complexity: O(2^n * 2^n) 131ms
+// Space Complexity: O(2^n) 50MB
+var maxProduct = function(s) {
+  let n = s.length, validMasks = [], maxPalinLen = 0;
+  for (let mask = 1; mask < (1 << n); mask++) {
+    let str = buildStr(mask);
+    if (isPalindrome(str)) {
+      validMasks.push([mask, str.length]);
+      maxPalinLen = Math.max(maxPalinLen, str.length);
+    }
+  }
+  
+  let maxProduct = 0;
+  for (let i = 0; i < validMasks.length; i++) {
+    if (validMasks[i][1] * maxPalinLen <= maxProduct) continue;
+    for (let j = i + 1; j < validMasks.length; j++) {
+      if ((validMasks[i][0] & validMasks[j][0]) === 0) {
+        maxProduct = Math.max(maxProduct, validMasks[i][1] * validMasks[j][1]);
+      }
+    }
+  }
+  return maxProduct;
+  
+  function buildStr(mask) {
+    let str = "";
+    for (let i = 0; i < n; i++) {
+      if ((mask >> i) & 1) {
+        str += s[i];
+      }
+    }
+    return str;
+  }
+  
+  function isPalindrome(str) {
+    let start = 0, end = str.length - 1;
+    while (start < end) {
+      if (str[start] !== str[end]) {
+        return false;
+      }
       start++, end--;
     }
     return true;
