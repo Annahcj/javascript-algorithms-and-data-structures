f9ae81e478f5c2c46cc8bfd8e7892284a84a945b
Anna An <annaan.golf@gmail.com>
2023-04-15 22:40:48 +1200

    Add solution

--- 2218.maximum-value-of-k-coins-from-piles.js
+++ 2218.maximum-value-of-k-coins-from-piles.js
@@ -4,7 +4,7 @@
 // Given a list piles, where piles[i] is a list of integers denoting the composition of the ith pile from top to bottom, and a positive integer k, return the maximum total value of coins you can have in your wallet if you choose exactly k coins optimally.
 
 
-// Solution: Knapsack - DP
+// Solution 1: Knapsack DP - Recursion w/ Memoization
 
 // dp[i][k] = maximum sum of taking k coins from the first i piles.
 
@@ -27,6 +27,30 @@ var maxValueOfCoins = function(piles, k) {
     }
     return dp[idx][k];
   }
+};
+
+// Solution 2: Bottom Up DP
+
+// dp[i][j] = maximum sum taking j coins from the first i piles
+// For each piles[i], populate each dp[i][j] (go through each j from 0 to k)
+// Then, try taking each amount of coins from the ith pile (0 to min(j, piles[i].length)).
+  // transition: dp[i][j] = max(dp[i][j], dp[i - 1][j - h])
+
+// Time Complexity: O(nk^2) 262ms
+// Space Complexity: O(nk) 56.2MB
+var maxValueOfCoins = function(piles, k) {
+  let n = piles.length, dp = Array(n).fill(0).map(() => Array(k + 1).fill(0));
+  for (let i = 0; i < n; i++) {
+    for (let j = 1; j <= k; j++) { // take j coins in total
+      dp[i][j] = i === 0 ? 0 : dp[i - 1][j]; // don't take any coins from ith pile
+      let sum = 0;
+      for (let h = 0; h < Math.min(j, piles[i].length); h++) { // take h + 1 coins from ith pile
+        sum += piles[i][h];
+        dp[i][j] = Math.max(dp[i][j], sum + (i === 0 ? 0 : dp[i - 1][j - h - 1]));
+      }
+    }
+  }
+  return dp[n - 1][k];
 };
 
 // Two test cases to run function on
