3e5fa4bd612eda08b6e5ccd2b08777a1cb5f9a8b
Anna An <annaan.golf@gmail.com>
2023-05-01 23:12:28 +1200

    Add solution

--- 1491.average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491.average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -19,3 +19,7 @@ var average = function(salary) {
   }
   return (sum - min - max) / (n - 2);
 };
+
+// Two test cases
+console.log(average([4000,3000,1000,2000])) // 2500
+console.log(average([1000,2000,3000])) // 2000
