e38cdb9c5258b8dfe04bd803879e7d0b5c6717dd
Anna An <annaan.golf@gmail.com>
2023-05-02 18:30:01 +1200

    Add solution

--- 1822.sign-of-the-product-of-an-array.js
+++ 1822.sign-of-the-product-of-an-array.js
@@ -7,27 +7,24 @@
 // Return signFunc(product).
 
 
-// Solution: Calculate Product
+// Solution: Logic
 
-// Keep a product variable set to 1 (if we set it to zero, whatever we multiply it by, it will remain 0)
-// Loop through nums (pointer = i)
-  // multiply product by nums[i]
-// - when iteration finishes -
-// If product is bigger than 0, return 1
-// If product is smaller than 0, return -1
-// If product is zero, return 0.
+// If nums contains at least one 0, the product will be 0.
+// If there is an even number of negative numbers, the product will be positive. 
+// If there is an odd number of negative numbers, the product will be negative.
 
-// Time Complexity: O(n) 72ms
-// Space Complexity: O(1) 40.1MB
-  var arraySign = function(nums) {
-    let product = 1;
-    for (var i = 0; i < nums.length; i++) product *= nums[i];
-    if (product > 0) return 1;
-    else if (product < 0) return -1;
-    return 0;
-  };
+// Time Complexity: O(n) 62ms
+// Space Complexity: O(1) 44.6MB
+var arraySign = function(nums) {
+  let negative = 0;
+  for (let num of nums) {
+    if (num === 0) return 0;
+    negative += num < 0 ? 1 : 0;
+  }
+  return negative % 2 === 1 ? -1 : 1;
+};
   
-  // Three test cases to run function on
-  console.log(arraySign([-1,-2,-3,-4,3,2,1])) // 1
-  console.log(arraySign([1,5,0,2,-3])) // 0
-  console.log(arraySign([-1,1,-1,1,-1])) // -1
+// Three test cases to run function on
+console.log(arraySign([-1,-2,-3,-4,3,2,1])) // 1
+console.log(arraySign([1,5,0,2,-3])) // 0
+console.log(arraySign([-1,1,-1,1,-1])) // -1
