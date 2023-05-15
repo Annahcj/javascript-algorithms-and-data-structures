26d9817e985096d8897d66fa05a3970ddef604fb
Anna An <annaan.golf@gmail.com>
2023-05-15 16:29:23 +1200

    Add solution

--- 1721.swapping-nodes-in-a-linked-list.js
+++ 1721.swapping-nodes-in-a-linked-list.js
@@ -3,33 +3,27 @@
 // Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
 
 
-// Solution: Two Pass
+// Solution: Two Pointers
 
-// 1. Get the length of the list
-// 2. Get the two nodes at positions k and n - k + 1
-// 3. Swap the values of the two nodes
+// Traverse through the list with two pointers (let's call them slow and fast nodes) k elements apart until the fast node reaches the end of the list.
+// Keep track of the current index in the list.
+// When the index is k, the fast node is the kth node, so we save the reference.
+// When the fast node reaches the end of the list, the slow node will be the n-kth node since they are k nodes apart.
+// Finally, swap the values of the kth node and the n-kth node.
 
-// Time Complexity: O(n) 745ms
-// Space Complexity: O(1) 101.9MB
+// Time Complexity: O(n) 413ms
+// Space Complexity: O(1) 101.6MB
 var swapNodes = function(head, k) {
-  // get the length
-  let n = 0, node = head;
-  while (node) {
-    n++;
-    node = node.next;
+  let slow = head, fast = head;
+  let i = 1, kth = null;
+  while (fast) {
+    if (i === k) kth = fast;
+    if (i > k) slow = slow.next;
+    fast = fast.next;
+    i++;
   }
-  // get the two nodes
-  let node1, node2, idx = 1;
-  node = head;
-  while (idx <= n) {
-    if (idx === k) node1 = node;
-    if (idx === n - k + 1) node2 = node;
-    node = node.next;
-    idx++;
-  }
-  // swap the values
-  let val = node1.val;
-  node1.val = node2.val;
-  node2.val = val;
+  let kthVal = kth.val;
+  kth.val = slow.val;
+  slow.val = kthVal;
   return head;
 };
