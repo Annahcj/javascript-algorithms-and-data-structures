8ea74a77fd508e897cbd2c526d7caa15ef79faa8
Anna An <annaan.golf@gmail.com>
2023-08-03 22:13:44 +1200

    Add solution

--- 17.letter-combinations-of-a-phone-number.js
+++ 17.letter-combinations-of-a-phone-number.js
@@ -1,46 +1,44 @@
 // 17. Letter Combinations of a Phone Number
 // Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
-// Solution 1: Backtracking
-// Map all letters to their corresponding numbers in a hashmap.
-// Create a recursive 'backtrack' function.
-  // This accepts an array and an index as params
-  // Until the length of the array is equal to the length of digits, we keep looping through all letters mapped to digit at index,
-  // for each, we push into array, call backtrack with arr and index + 1, then pop off the last digit.
-  // When a complete combination is reached, we push it into our result array, then return from the recursive call.
-// Call the backtrack function for an empty array and index of 0.
+// Solution: Backtracking
 
-// Time Complexity: O(4^n * n) 76ms
-// Space Complexity: O(n) (maximum recursion depth is 4) 38.8MB
+// Use backtracking to find each combination.
+// For each backtrack(index, comb), go through each letter for the ith digit and add it as the next letter in the current string.
+
+// n = length of digits
+// Time Complexity: O(4^n) 51ms
+// Space Complexity: O(n) (max recursive depth is 4) 41.8MB
 var letterCombinations = function(digits) {
-    if (!digits.length) return [];
-    let buttons = {
-      2: ['a', 'b', 'c'],
-      3: ['d', 'e', 'f'],
-      4: ['g', 'h', 'i'],
-      5: ['j', 'k', 'l'],
-      6: ['m', 'n', 'o'],
-      7: ['p', 'q', 'r', 's'],
-      8: ['t', 'u', 'v'],
-      9: ['w', 'x', 'y', 'z']
-    };
-    let combinations = [];
-    backTrack([], 0);
-    function backTrack(arr, index) {
-      if (digits.length === arr.length) {
-        combinations.push(arr.join(""));
-        return;
-      }
-      buttons[digits[index]].forEach(comb => {
-        arr.push(comb);
-        backTrack(arr, index + 1);
-        arr.pop();
-      });
-    }
-    return combinations;
+  if (!digits.length) return [];
+  let letters = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z']
   };
-  
-  // Three test cases to run function on
-  console.log(letterCombinations("23")) // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-  console.log(letterCombinations("")) // []
-  console.log(letterCombinations("2")) // ["a","b","c"]
+  let combinations = [];
+  backtrack(0, []);
+  return combinations;
+
+  function backtrack(index, comb) {
+    if (comb.length === digits.length) {
+      combinations.push(comb.join(""));
+      return;
+    }
+    for (let letter of letters[digits[index]]) {
+      comb.push(letter);
+      backtrack(index + 1, comb);
+      comb.pop();
+    }
+  }
+};
+
+// Three test cases 
+console.log(letterCombinations("23")) // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+console.log(letterCombinations("")) // []
+console.log(letterCombinations("2")) // ["a","b","c"]
