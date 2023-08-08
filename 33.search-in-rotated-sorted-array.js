6fbe3a15899750a03ec4d3df62590fa66c4c0d0b
Anna An <annaan.golf@gmail.com>
2023-08-08 22:51:27 +1200

    Add solution

--- 33.search-in-rotated-sorted-array.js
+++ 33.search-in-rotated-sorted-array.js
@@ -4,57 +4,46 @@
 // Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
 
-// Solution 1: Tweaked Binary Search
+// Solution: Modified Binary Search
 
-// Thoughts:
-// There can be two scenarios:
-// 1: The left half of the array is in correct order (from start to mid point) -> for e.g: [4,5,6,7,1,2,3] 
-// 2: The left half of the array is not in correct order -> for e.g: [5,6,1,2,3,4]
+// For each mid index, we have three cases:
+  // 1. nums[mid] is the target
+  // 2. target is in* the left subarray (low, mid - 1)
+  // 3. target is in* the right subarray (mid + 1, high)
 
-// To solve this, we can test for both scenarios, then check for each possibility.
-// Scenario 1: using [4,5,6,7,1,2,3]
-// If mid (7) is bigger than target and start (4) is bigger than target, we must look in the right part of the array 
-// If mid is smaller than target, (since anything to the left of mid is always smaller), we must look at the right part.
-// Else, look in left part of array. 
+// To check whether a subarray has the correct number range for the target, we have two cases:
+  // 1. The subarray is increasing (nums[start] < nums[end]) e.g: [1,2,3,4]
+    // We are within range if the target is between nums[start] and nums[end] - e.g: [1,2,3,4], target = 2
+  // 2. The subarray is not increasing (nums[start] > nums[end]) e.g: [6,7,1,2]
+    // We are within range if nums[start] >= target OR nums[end] <= target - e.g: [6,7,1,2], target = 7 or 2
 
-// Scenario 2: using [5,6,1,2,3,4], target = 2
-// If mid (1) is smaller than target and start (5) is bigger than target, we must look right.
-// Else, look in left part of array.
+// * in the subarray meaning the number range is correct. It may not actually exist in the subarray, but that is the correct subarray if it exists.
 
-// Algorithm:
-// Set two pointers, start and end.
-// Loop while start is smaller than or equal to end
-  // Find middle element -> Math.floor((start + end) / 2)
-  // If mid element is target, return mid.
-  // If left half of array is sorted
-    // If mid element is smaller than target OR mid element is bigger than target and start is bigger than target -> set start to mid + 1.
-    // Else, set end to mid - 1.
-  // Else (left half is not sorted)
-    // If mid element is smaller than target AND start is bigger than target -> set start to mid + 1
-    // Else, set end to mid - 1.
-// If while loop finishes, return -1 (not found).
+// Time Complexity: O(log(n)) 44ms
+// Space Complexity: O(1) 42MB
+var search = function(nums, target) {
+  let n = nums.length, low = 0, high = n - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] === target) return mid;
+    if (correctSubarray(nums, low, mid - 1, target)) high = mid - 1;
+    else low = mid + 1;
+  }
+  return -1;
+};
 
-// Time Complexity: O(log(n)) 72ms
-// Space Complexity: O(1) 39.9MB
-  var search = function(nums, target) {
-    let start = 0, end = nums.length - 1;
-    while (start <= end) {
-      let mid = Math.floor((start + end) / 2);
-      if (nums[mid] === target) return mid;
-      if (nums[start] <= nums[mid]) {
-        if (nums[mid] < target || (nums[mid] > target && nums[start] > target)) start = mid + 1;
-        else end = mid - 1;
-      } else {
-        if (nums[mid] < target && nums[start] > target) start = mid + 1;
-        else end = mid - 1;
-      }
-    }  
-    return -1;
-  };
+function correctSubarray(nums, start, end, target) {
+  if (start >= end) return nums[start] === target;
+  if (nums[start] < nums[end]) {
+    return nums[start] <= target && nums[end] >= target;
+  } else {
+    return nums[end] >= target || nums[start] <= target;
+  }
+}
   
-  // Five test cases to run function on
-  console.log(search([6,7,8,9,10,11,1,2,3,4,5], 5)) // 3
-  console.log(search([5,1,2,3,4], 3)) // 3
-  console.log(search([4,5,6,7,0,1,2], 0)) // 4
-  console.log(search([4,5,6,7,0,1,2], 3)) // -1
-  console.log(search([1], 0)) // -1
+// Five test cases 
+console.log(search([6,7,8,9,10,11,1,2,3,4,5], 5)) // 3
+console.log(search([5,1,2,3,4], 3)) // 3
+console.log(search([4,5,6,7,0,1,2], 0)) // 4
+console.log(search([4,5,6,7,0,1,2], 3)) // -1
+console.log(search([1], 0)) // -1
