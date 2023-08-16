750a4995d00980e9b5d661a61c167356e631ba78
Anna An <annaan.golf@gmail.com>
2023-08-16 20:59:18 +1200

    Add solution

--- 239.sliding-window-maximum.js
+++ 239.sliding-window-maximum.js
@@ -3,47 +3,90 @@
 // Return the max sliding window.
 
 
-// Solution: Monotonic Queue
+// Solution: Monotonic Decreasing Deque
 
-// We can use a decreasing monotonic queue.
-// How a monotonic queue works: 
-// To push in an item: pop off all items until we find an item that is smaller than the one we are inserting, then we push in the new number.
-// This way, the first item in the queue is always the biggest.
-// When the window moves out of range (the first item in queue is left-most in the window), we shift it off the queue.
+// Maintain a monotonic decreasing deque.
+// For each nums[i], 
+  // Remove expired elements (gone outside the window) from the start of the deque (gone outside the window).
+  // Remove elements from the end of the deque that are smaller than nums[i] (there is no point keeping smaller elements that have a higher chance of leaving the window sooner)
+  // Push nums[i] to the end of the deque.
+  // The element at the start of the deque is the maximum element in the current window.
 
-// e.g: nums = [1,3,-1,-3,5,3,6,7], k = 3
-// 0 - queue: [1], res = []
-// 1 - queue: [3], res = []
-// 2 - queue: [3, -1], res = [3]
-// 3 - queue: [3, -1, -3], res = [3, 3]
-// 4 - queue: [5], res = [3, 3, 5]
-// 5 - queue: [5, 3], res = [3, 3, 5, 5]
-// 6 - queue: [6], res = [3, 3, 5, 5, 6]
-// 7 - queue: [7], res = [3, 3, 5, 5, 6, 7]
-
-// Algorithm:
-// Keep a queue (the monotonic queue), and an array 'res' for the results.
-// Loop through nums (pointer = i)
-  // Loop through the queue while the last item of the queue is smaller than nums[i]
-    // Keep popping last item off queue
-  // Push nums[i] into the queue.
-  // if the leftmost element in the queue is out of range (equal to i - k), shift it off the queue.
-  // if i is bigger than or equal to k - 1, push nums[queue[0]] to res. 
-// Return res.
-
-// Time Complexity: O(n) 428ms
-// Space Complexity: O(k) 79.1MB (monotonic queue's length will never be bigger than k)
+// Time Complexity: O(n) 258ms
+// Space Complexity: O(k) (excluding output) 85.8MB
 var maxSlidingWindow = function(nums, k) {
-  let queue = [], res = [];
-  for (var i = 0; i < nums.length; i++) {
-    while (queue.length && nums[queue[queue.length - 1]] <= nums[i]) queue.pop();
-    queue.push(i);
-    if (queue[0] === i - k) queue.shift();
-    if (i >= k - 1) res.push(nums[queue[0]]);
-  } 
-  return res;
+  let n = nums.length, deque = new Deque(), ans = Array(n - k + 1);
+  for (let i = 0; i < n; i++) {
+    while (!deque.isEmpty() && i - deque.front() >= k) deque.shift();
+    while (!deque.isEmpty() && nums[deque.back()] <= nums[i]) deque.pop();
+    deque.push(i);
+    if (i >= k - 1) ans[i - k + 1] = nums[deque.front()];
+  }
+  return ans;
 };
+
+class Deque {
+  constructor() {
+    this.head = new Node(null);
+    this.tail = new Node(null);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+    this.size = 0;
+  }
+  unshift(val) {
+    let node = new Node(val);
+    node.next = this.head.next;
+    node.prev = this.head;
+    this.head.next.prev = node;
+    this.head.next = node;
+    this.size++;
+  }
+  push(val) {
+    let node = new Node(val);
+    node.prev = this.tail.prev;
+    node.next = this.tail;
+    this.tail.prev.next = node;
+    this.tail.prev = node;
+    this.size++;
+  }
+  shift() {
+    let head = this.head.next;
+    this.removeNode(head);
+    this.size--;
+    return head.val;
+  }
+  pop() {
+    let tail = this.tail.prev;
+    this.removeNode(tail);
+    this.size--;
+    return tail.val;
+  }
+  removeNode(node) {
+    if (!node.prev && !node.next) return;
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    node.prev = null;
+    node.next = null;
+  }
+  front() {
+    return this.head.next.val;
+  }
+  back() {
+    return this.tail.prev.val;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
   
-// Two test cases to run function on
+// Two test cases 
 console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)) // [3,3,5,5,6,7]
 console.log(maxSlidingWindow([1,-1], 1)) // [1, -1]
