bc1a0ee81597972d05928a1ed84ae7d6ee84f3b6
Anna An <annaan.golf@gmail.com>
2023-09-30 22:54:35 +1300

    Add solution

--- 456.132-pattern.js
+++ 456.132-pattern.js
@@ -3,44 +3,30 @@
 // Return true if there is a 132 pattern in nums, otherwise, return false.
 
 
-// Solution: Decreasing Stack
+// Solution: Monotonic Decreasing Stack
 
-// 1. Populate an array min, where min[i] = minimum number in nums in the range of [0, ..., i]
-// 2. Keep a monotonic decreasing stack and loop from back to front.
-  // Try each nums[i] as the middle number.
-  // The 3rd number must be larger than the 1st number, so if nums[i] <= min[i], skip it.
-  // Pop out numbers that are not valid as 3rd numbers (too small). 
-    // This works because the values of min will only get bigger or stay the same as we continue to the left.
-  // At this point, stack[top] will be the smallest possible number larger than min[i].
-  // Check whether it is a valid 132 pattern: if stack[top] < nums[i]
-    // We know that all values in the stack must be >= min[i]
-    // So if stack[top] < nums[i], we have found a 132 pattern.
+// Find a pair (j, k) with maximum nums[j], where j < k and nums[j] > nums[k].
+// Use a monotonic decreasing stack to keep track of potential nums[k]. 
+  // Pop nums[k] from the top of the stack while nums[j] > nums[k].
+  // Then we push the current nums[j] to the end of the stack as a future nums[k].
+  // Record the largest nums[k] so far that we have popped from the stack.
+// If the current maximum nums[k] > the current nums[i], we have found a 132 pattern and can return true.
 
-// Why the stack is always decreasing:
-  // If a number is still in the stack, that means it is larger than min[i].
-  // Otherwise it will be popped off.
-  // Then, if the condition is satisfied, we will return true.
-  // A smaller number 3rd that satisfies the condition of 1st < 3rd will always return first.
-
-// Time Complexity: O(n) 122ms
-// Space Complexity: O(n) 54.4MB
+// Time Complexity: O(n) 79ms
+// Space Complexity: O(n) 54.1MB
 var find132pattern = function(nums) {
-  let n = nums.length, min = Array(n);
-  min[0] = nums[0];
-  for (let i = 1; i < n; i++) {
-    min[i] = Math.min(min[i - 1], nums[i]);
-  }
-  
-  let stack = [];
-  for (let i = n - 1; i > 0; i--) {
-    if (nums[i] <= min[i]) continue; 
-    while (stack.length && stack[stack.length - 1] <= min[i]) stack.pop();
-    if (stack.length && stack[stack.length - 1] < nums[i]) return true;
-    stack.push(nums[i]);
+  let n = nums.length, stack = [], maxK = -Infinity;
+  for (let i = n - 1; i >= 0; i--) {
+    if (maxK > nums[i]) return true; 
+    // now we treat nums[i] as nums[j]
+    while (stack.length && nums[i] > stack[stack.length - 1]) {
+      maxK = Math.max(maxK, stack.pop());
+    }
+    stack.push(nums[i]); // now nums[i] is treated as a future nums[k]
   }
   return false;
 };
 
-// Two test cases to run function on
+// Two test cases 
 console.log(find132pattern([1,2,3,4])) // false
 console.log(find132pattern([3,1,4,2])) // true
