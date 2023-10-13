d99d9c33e22987dedba3fc9cb2f1f9301db7121c
Anna An <annaan.golf@gmail.com>
2023-10-13 22:18:49 +1300

    Add solution

--- 746.min-cost-climbing-stairs.js
+++ 746.min-cost-climbing-stairs.js
@@ -4,7 +4,7 @@
 // Return the minimum cost to reach the top of the floor.
 
 
-// Solution: Bottom-up Dynamic Programming
+// Solution 1: Bottom-up Dynamic Programming
 
 // The top floor is position n (cost.length).
 // Create a dp array the size of n.
@@ -17,15 +17,33 @@
 // Time Complexity: O(n) 80ms
 // Space Complexity: O(n) 40.7MB
 var minCostClimbingStairs = function(cost) {
-  let n = cost.length;
-  let dp = Array(n);
+  let n = cost.length, dp = Array(n);
   dp[0] = cost[0], dp[1] = cost[1];
-  for (var i = 2; i < n; i++) {
+  for (let i = 2; i < n; i++) {
     dp[i] = Math.min(dp[i - 2], dp[i - 1]) + cost[i];
   }
   return Math.min(dp[i - 2], dp[i - 1]);
 };
 
-// Two test cases to run function on
+// Solution 2: Constant Space
+
+// Notice that we only need to access the results from the last two steps.
+// We can use two variables to keep track of the previous and previous previous costs. 
+
+// Time Complexity: O(n) 54ms
+// Space Complexity: O(1) 43.9MB
+var minCostClimbingStairs = function(cost) {
+  let prevPrev = cost[0], prev = cost[1];
+  cost.push(0);
+  
+  for (let i = 2; i < cost.length; i++) {
+    let curr = Math.min(prevPrev, prev) + cost[i];
+    prevPrev = prev;
+    prev = curr;
+  }
+  return prev;
+};
+
+// Two test cases
 console.log(minCostClimbingStairs([10,15,20])) // 15
 console.log(minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])) // 6
