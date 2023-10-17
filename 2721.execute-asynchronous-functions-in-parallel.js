7728bffbe0bacb2389a4ad0bf58aa7765823dea7
Anna An <annaan.golf@gmail.com>
2023-10-17 21:56:53 +1300

    Add solution

--- 2721.execute-asynchronous-functions-in-parallel.js
+++ 2721.execute-asynchronous-functions-in-parallel.js
@@ -7,26 +7,50 @@
 // Please solve it without using the built-in Promise.all function.
 
 
-// Solution: 
+// Solution 1: forEach loop and async/await
+ 
+// Use a forEach loop to run each function in parallel.
+// Count the number of promises that have been resolved and return the array of results if all have been resolved.
+// Use async/await together with try/catch.
 
+var promiseAll = function(functions) {
+  return new Promise((resolve, reject) => {
+    let n = functions.length, results = Array(n);
+    let resolved = 0;
+    
+    functions.forEach(async (fn, index) => {
+      try {
+        const result = await fn();
+        results[index] = result;
+        resolved++;
+        if (resolved === n) resolve(results);
+      } catch (error) {
+        reject(error);
+      }
+    })
+  })
+};
+
+
+// Solution 2: .then()/.catch()
+ 
 // Use .then() to resolve each promise and .catch() to catch any errors.
-// By looping through each function and using .then(), we will process them in parallel.
+// By looping through each function using a forEach and using .then(), we will process them in parallel.
 // Keep track of the count of resolved promises and only resolve with the array of results when the count is equal to the number of functions.
 
-var promiseAll = async function(functions) {
+var promiseAll = function(functions) {
   return new Promise((resolve, reject) => {
-    let results = Array(functions.length), finished = 0;
-    for (let i = 0; i < functions.length; i++) {
-      functions[i]()
+    let n = functions.length, results = Array(n);
+    let resolved = 0;
+    
+    functions.forEach(async (fn, index) => {
+      fn
         .then((result) => {
-          results[i] = result;
-          if (++finished === functions.length) {
-            resolve(results);
-          }
-        })
-        .catch((err) => {
-          reject(err);
-        })
-    }
+        results[index] = result;
+        resolved++;
+        if (resolved === n) resolve(results);
+      })
+        .catch((error) => reject(error))
+    })
   })
 };
