77b143f186c1257f8588c7889c63ada6585fe67e
Anna An <annaan.golf@gmail.com>
2023-10-17 21:58:21 +1300

    Add solution

--- 1361.validate-binary-tree-nodes.js
+++ 1361.validate-binary-tree-nodes.js
@@ -4,7 +4,7 @@
 // Note that the nodes have no values and that we only use the node numbers in this problem.
 
 
-// Solution: DFS
+// Solution 1: DFS
 
 // Properties of a valid binary tree:
   // Each node has no more than 2 children.
@@ -46,6 +46,49 @@ var validateBinaryTreeNodes = function(n, leftChild, rightChild) {
     }
     return true;
   }
+};
+
+
+// Solution 2: BFS
+
+// 1. Find the root of the tree (the node with no incoming edges).
+// 2. Starting from the root, use BFS to visit each node and check that no nodes are visited more than once, and each node is visited exactly once.
+
+// Time Complexity: O(n) 70ms
+// Space Complexity: O(n) 46.9MB
+var validateBinaryTreeNodes = function(n, leftChild, rightChild) {
+  let indegrees = Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    if (leftChild[i] !== -1) indegrees[leftChild[i]]++;
+    if (rightChild[i] !== -1) indegrees[rightChild[i]]++;
+  }
+  let roots = 0, root = -1;
+  for (let i = 0; i < n; i++) {
+    if (indegrees[i] === 0) {
+      roots++;
+      root = i;
+    }
+  }
+  if (roots !== 1) return false; // more than one node with no indegrees: disconnected tree, or zero nodes with no indegrees: cycle
+  
+  let queue = [root], seen = Array(n).fill(false), visitedCount = 0;
+  seen[root] = true;
+  while (queue.length) {
+    let node = queue.shift();
+    visitedCount++;
+    
+    if (leftChild[node] !== -1) {
+      if (seen[leftChild[node]]) return false; // cycle
+      seen[leftChild[node]] = true;
+      queue.push(leftChild[node]);
+    }
+    if (rightChild[node] !== -1) {
+      if (seen[rightChild[node]]) return false; // cycle
+      seen[rightChild[node]] = true;
+      queue.push(rightChild[node]);
+    }
+  }
+  return visitedCount === n;
 };
 
 // Three test cases
