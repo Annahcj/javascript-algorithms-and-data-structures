fee7da40bb3bb290cce617846bd51303d7b75009
Anna An <annaan.golf@gmail.com>
2023-10-18 21:16:15 +1300

    Add solution

--- 2050.parallel-courses-III.js
+++ 2050.parallel-courses-III.js
@@ -6,48 +6,46 @@
 // Return the minimum number of months needed to complete all the courses.
 
 
-// Solution: Topological Sort 
+// Solution: Topological Sort
 
-// 1. Create indegrees array filled with 0's (size of n + 1, since they are 1-indexed)
-// 2. Build reversed graph (pre -> course), and count indegrees for each course. 
-// 3. Create DP array 'maxTimes' (size of n + 1, filled with -Infinity) which indicates the max time to complete the ith course.
-// 4. Create Queue, push all indegree 0 courses into queue, and set maxtime of indegree 0 courses to their time in the time array.
-// 5. Traverse Courses in Topological Order, also update the maxTime of neighboring courses (pre -> course)
-// 6. Return the maximum time in the maxTime array
+// Perform topological sort and keep track of the maximum number of months it takes to reach each node.
+// months[i] = maximum months to get to node i, including node i
 
-// n = number of courses, m = number of relations
-// Time Complexity: O(n + m) 406ms
-// Space Complexity: O(n + m) 85.8MB
+// n = number of nodes, m = number of edges
+// Time Complexity: O(n + m) 220ms
+// Space Complexity: O(n + m) 105.5MB
 var minimumTime = function(n, relations, time) {
-  let graph = {};
-  let indegrees = Array(n + 1).fill(0);
-  for (var [pre, course] of relations) {
-    if (!graph[pre]) graph[pre] = [];
-    graph[pre].push(course);
-    indegrees[course]++;
+  let indegrees = Array(n + 1).fill(0), graph = Array(n + 1).fill(0).map(() => []);
+  for (let i = 0; i < relations.length; i++) {
+    let [prevCourse, nextCourse] = relations[i];
+    indegrees[nextCourse]++;
+    graph[prevCourse].push(nextCourse);
   }
-  let maxTimes = Array(n + 1).fill(-Infinity);
   let queue = [];
-  for (var i = 1; i <= n; i++) {
-    if (indegrees[i] === 0) {
-      queue.push(i);
-      maxTimes[i] = time[i - 1];
-    }
-  } 
+  for (let i = 1; i <= n; i++) {
+    if (indegrees[i] === 0) queue.push(i);
+  }
+  
+  let months = Array(n + 1).fill(0); // months[i] = maximum months to get to node i, including node i
+  for (let rootNode of queue) {
+    months[rootNode] = time[rootNode - 1];
+  }
+  let maxMonths = 0;
   while (queue.length) {
-    let currCourse = queue.shift();
-    for (var course of (graph[currCourse] || [])) {
-      maxTimes[course] = Math.max(maxTimes[course], time[course - 1] + maxTimes[currCourse]);
-      indegrees[course]--;
-      if (indegrees[course] === 0) queue.push(course);
+    let node = queue.shift();
+    maxMonths = Math.max(maxMonths, months[node]);
+    for (let nei of graph[node]) {
+      indegrees[nei]--;
+      if (indegrees[nei] === 0) {
+        queue.push(nei);
+      }
+      months[nei] = Math.max(months[nei], months[node] + time[nei - 1]);
     }
   }
-  let max = -Infinity;
-  for (var t of maxTimes) max = Math.max(max, t);
-  return max;
+  return maxMonths;
 };
 
-// Four test cases to run function on
+// Four test cases
 console.log(minimumTime(2, [[2,1]], [10000,10000])) // 20000
 console.log(minimumTime(3, [[1,3],[2,3]], [3,2,5])) // 8
 console.log(minimumTime(5, [[1,5],[2,5],[3,5],[3,4],[4,5]], [1,2,3,4,5])) // 12
