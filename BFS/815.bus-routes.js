347bd6b594fed9d6891be950cafbf52653c35c08
Anna An <annaan.golf@gmail.com>
2023-11-12 22:18:11 +1300

    Add solution

--- BFS/815.bus-routes.js
+++ BFS/815.bus-routes.js
@@ -5,52 +5,92 @@
 // Return the least number of buses you must take to travel from source to target. Return -1 if it is not possible.
 
 
-// Solution: BFS
+// Solution 1: BFS over Stops
 
-// Build the graph like : [busStop: [bus, bus, ...], busStop: [bus, ...]]
-// From stops to buses, not the other way around.
-// Create a queue with the initial [source, 0] (meaning [starting stop, number of steps])
-// Keep a set to make sure we don't visit the same stop twice
-// Loop while the queue is not empty
-  // Shift out [stop, buses] from the queue
-  // If stop is equal to the target, return buses.
-  // Loop over each bus in graph[stop]
-    // loop over each route (actually a stop) in routes[stop]
-      // if route has not been visited yet,
-        // push [route, buses + 1] to the queue
-        // mark route as visited
-    // clear out routes[stop] (time optimization, so that we don't loop over this again)
-// If we didn't find a path, return -1.
+// 1. Create a map of stops to buses: {bus stop: [bus, bus, ...], bus stop: [bus, ...], ...}
+// 2. Use BFS to find the minimum number of buses to take from bus stop source to target.
+  // Use a hashset to keep track of stops we have visited already, and avoid revisiting them.
+  // When we reach a stop, go through each bus that is connected to the current stop, then go through each stop connected to each bus.
 
-// n = routes.length, m = routes[i].length
-// Time Complexity: O(nm) 728ms
-// Space Complexity: O(nm) 74.3B
+// n = number of buses, m = number of bus stops
+// Time Complexity: O(m^2 * n) 579ms
+// Space Complexity: O(n + m) 114.8MB
 var numBusesToDestination = function(routes, source, target) {
-  let graph = {};
-  for (var i = 0; i < routes.length; i++) {
-    for (var route of routes[i]) {
-      if (!graph[route]) graph[route] = [];
-      graph[route].push(i);
+  let stopToBusMap = {};
+  for (let bus = 0; bus < routes.length; bus++) {
+    for (let stop of routes[bus]) {
+      if (!stopToBusMap[stop]) stopToBusMap[stop] = [];
+      stopToBusMap[stop].push(bus);
     }
   }
-  console.log(graph, source)
-  let queue = [[source, 0]], seen = new Set([source]);
+  let queue = [source], buses = 0;
+  let seen = new Set([source]);
   while (queue.length) {
-    let [stop, buses] = queue.shift();
-    if (stop === target) return buses;
-    for (var bus of graph[stop]) { // loop over each bus in graph[stop]
-      for (var route of routes[bus]) { // loop over each stop in routes[bus]
-        if (!seen.has(route)) { // if stop/route hasn't been visited before
-          queue.push([route, buses + 1]); 
-          seen.add(route); // mark as seen
+    for (let i = queue.length - 1; i >= 0; i--) {
+      let stop = queue.shift();
+      if (stop === target) return buses;
+      for (let bus of stopToBusMap[stop]) {
+        for (let nextStop of routes[bus]) {
+          if (!seen.has(nextStop)) {
+            queue.push(nextStop);
+            seen.add(nextStop);
+          }
         }
+        routes[bus] = [];
       }
-      routes[bus] = []; // clear routes[bus] so that we won't loop over them again
     }
+    buses++;
   }
   return -1;
 };
 
-// Two test cases to run function on
+
+// Solution 2: BFS over Buses
+
+// 1. Map buses to stops: {bus stop: [bus, bus, bus], ...}
+// 2. Map buses to a hashset of buses it's connected to.
+// 3. Use level-by-level BFS to find the minimum number of buses to reach each bus (not bus stop).
+  // Initial the queue with buses directly connected to the source bus stop.
+  // We have found a path when we reach a bus directly connected to the target bus stop.
+
+// n = number of buses, m = number of bus stops
+// Time Complexity: O(m * n^2) 656ms
+// Space Complexity: O(n + m) 112.5MB
+var numBusesToDestination = function(routes, source, target) {
+  if (source === target) return 0;
+  let n = routes.length, stopToBusMap = {}; // {bus stop: [bus, bus, bus], ...}
+  for (let bus = 0; bus < n; bus++) {
+    for (let stop of routes[bus]) {
+      if (!stopToBusMap[stop]) stopToBusMap[stop] = [];
+      stopToBusMap[stop].push(bus);
+    }
+  }
+  let busMap = Array(n).fill(0).map(() => new Set());
+  for (let stop in stopToBusMap) {
+    for (let bus of stopToBusMap[stop]) {
+      for (let nextBus of stopToBusMap[stop]) {
+        busMap[bus].add(nextBus);
+      }
+    }
+  }
+  let queue = [...stopToBusMap[source]], buses = 1;
+  let seen = Array(n).fill(false), targetBuses = new Set(stopToBusMap[target]);
+  for (let bus of queue) seen[bus] = true;
+  while (queue.length) {
+    for (let i = queue.length - 1; i >= 0; i--) {
+      let bus = queue.shift();
+      if (targetBuses.has(bus)) return buses;
+      for (let nextBus of busMap[bus]) {
+        if (seen[nextBus]) continue;
+        seen[nextBus] = true;
+        queue.push(nextBus);
+      }
+    }
+    buses++;
+  }
+  return -1;
+};
+
+// Two test cases
 console.log(numBusesToDestination([[1,2,7],[3,6,7]], 1, 6)) // 2
 console.log(numBusesToDestination([[7,12],[4,5,15],[6],[15,19],[9,12,13]], 15, 12)) // -1
