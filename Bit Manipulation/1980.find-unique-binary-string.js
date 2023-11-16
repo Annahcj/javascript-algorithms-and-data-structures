739566b8c2393725dc0f44dae4e406ca42ed9548
Anna An <annaan.golf@gmail.com>
2023-11-16 21:55:17 +1300

    Add solution

--- Bit Manipulation/1980.find-unique-binary-string.js
+++ Bit Manipulation/1980.find-unique-binary-string.js
@@ -18,7 +18,32 @@ var findDifferentBinaryString = function(nums) {
 };
 
 
-// Solution 2: Flip ith bit of the ith string
+// Solution 2: Enumerate Powers of Two
+
+// Since there are n strings that each contain only n bits, we can always find one power of two (including 0 and 1) that doesn't appear in nums.
+
+// e.g: n = 5
+  // 00000
+  // 10000
+  // 01000
+  // 00100
+  // 00010
+  // 00001
+
+// Time Complexity: O(n^2) 54ms
+// Space Complexity: O(n^2) 42MB
+var findDifferentBinaryString = function(nums) {
+  let n = nums.length, numsSet = new Set(nums);
+  let zero = '0'.repeat(n);
+  if (!numsSet.has(zero)) return zero;
+  for (let i = 0; i < n; i++) {
+    let str = `${'0'.repeat(i)}1${'0'.repeat(n - i - 1)}`;
+    if (!numsSet.has(str)) return str;
+  }
+};
+
+
+// Solution 3: Flip ith bit of the ith string
 
 // Flip the ith bit of the ith string.
 // This ensures the final string will not be the same as any of the binary strings since at least one bit is different.
