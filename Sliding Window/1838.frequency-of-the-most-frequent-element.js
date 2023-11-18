70539039e2f0c076cebd3938bf8255a5936ffa59
Anna An <annaan.golf@gmail.com>
2023-11-18 22:55:26 +1300

    Add solution

--- Sliding Window/1838.frequency-of-the-most-frequent-element.js
+++ Sliding Window/1838.frequency-of-the-most-frequent-element.js
@@ -4,34 +4,34 @@
 // Return the maximum possible frequency of an element after performing at most k operations.
 
 
-// Solution: Sort & Sliding Window
+// Solution: Sliding Window & Sorting
 
-// For all elements within a window, the difference of e.g: [1,2,4] with [4,4,4] (turning 1 and 2 into 4) must be less than or equal to k.
+// First, sort nums in asc order.
+// Maintain a sliding window with two pointers (i, j), where the sum of differences of all elements compared to nums[j] is at most k.
 
-// e.g: [1,2,4], k = 2.
-// 0: sum = 1, sum + k is greater than 1 * 1. Max freq = 1
-// 1: sum = 3, sum + k is greater than 2 * 2. Max freq = 2
-// 2: sum = 7,  
-  // 7 + k is less than 4 * 3. Move left pointer up, subtract nums[i] from sum. Sum = 6.
-  // 6 + k is equal to 4 * 2. Max freq = 2.
+// Move index j up incrementally, and update the difference sum every time we increase j: diffSum += (nums[j] - nums[j - 1]) * window size
+  // Explanation: Every element will gain a difference of nums[j] - nums[j - 1] since we move j up by 1.
+// Move up index i while the difference sum exceeds k.
+// Record the maximum window size.
 
-// ans = 2.
-
-// Time Complexity: O(n log(n)) 258ms
-// Space Complexity: O(log(n)) (space for sorting) 57.4MB
+// Time Complexity: O(n log(n)) 188ms
+// Space Complexity: O(log(n)) (space for sorting) 56.8MB
 var maxFrequency = function(nums, k) {
   nums.sort((a, b) => a - b);
-  
-  let i = 0, n = nums.length;
-  let sum = 0, ans = 1;
-  for (let j = 0; j < n; j++) {
-    sum += nums[j];
-    while (sum + k < nums[j] * (j - i + 1)) { 
-      sum -= nums[i++];
+  let n = nums.length, diffSum = 0, maxFreq = 1;
+  for (let j = 0, i = 0; j < n; j++) {
+    if (j > 0) {
+      let diff = nums[j] - nums[j - 1];
+      let windowSize = j - i;
+      diffSum += diff * windowSize;
     }
-    ans = Math.max(ans, j - i + 1);
+    while (diffSum > k) {
+      diffSum -= (nums[j] - nums[i]);
+      i++;
+    }
+    maxFreq = Math.max(maxFreq, j - i + 1);
   }
-  return ans;
+  return maxFreq;
 };
 
 // Two test cases
