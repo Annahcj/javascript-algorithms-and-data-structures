2999c418dd0856e4d50be3b329be83069c66780a
Anna An <annaan.golf@gmail.com>
2023-11-29 21:19:10 +1300

    Add solution

--- Bit Manipulation/191.number-of-1-bits.js
+++ Bit Manipulation/191.number-of-1-bits.js
@@ -4,24 +4,18 @@
 
 // Solution: Check Each Bit
 
-// Use a mask to check each bit of n.
-// Set mask to 1 initially -> 00000000000000000000000000000001
-// Loop 32 times (for 32 bits) 
-  // if (mask & n) is not 0, increment count (mask will always be 1 at the bit we are checking, so if answer is 1, bit at n will also be 1, otherwise 0)
-  // left shift the one bit in mask -> (the one will move left by 1 position)
-// Return count.
+// Go through each bit and count the '1' bits.
 
-// Time Complexity: O(1) 88ms
-// Space Complexity: O(1) 40.3MB
+// Time Complexity: O(1) 55ms
+// Space Complexity: O(1) 41.8MB
 var hammingWeight = function(n) {
-  let mask = 1, count = 0;
-  for (var i = 0; i < 32; i++) {
-    if (mask & n) count++;
-    mask <<= 1;
-  } 
-  return count; 
+  let count = 0;
+  for (let i = 0; i < 32; i++) {
+    count += (n >> i) & 1;
+  }
+  return count;
 };
 
-// Two test cases to run function on
+// Two test cases
 console.log(hammingWeight(11)) // 3
 console.log(hammingWeight(128)) // 1
