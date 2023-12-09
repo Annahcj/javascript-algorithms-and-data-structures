ad3975933b6e964827ec1eed550c858092a8e196
Anna An <annaan.golf@gmail.com>
2023-12-09 22:21:53 +1300

    Add solution

--- DFS/94.binary-tree-inorder-traversal.js
+++ DFS/94.binary-tree-inorder-traversal.js
@@ -2,7 +2,7 @@
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
 
-// Solution: Recursive In-Order Traversal
+// Solution 1: Recursive In-Order Traversal
 
 // Go as left as possible, then take the value of the node, then go as right as possible.
 
@@ -20,3 +20,36 @@ var inorderTraversal = function(root) {
     dfs(node.right);
   }  
 };
+
+
+// Solution 2: Iterative DFS w/ Stack
+
+// Use a stack to simulate the recursive stack.
+
+// To start, traverse as left as possible from the root node and push those nodes into a stack.
+// 1. Pop the last node off the stack.
+// 2. Add that node's value to the inorder traversal.
+// 3. Traverse as left as possible from the node's right child.
+// 4. Repeat this until the stack becomes empty.
+
+// Time Complexity: O(n) 53ms
+// Space Complexity: O(n) 41.3MB
+var inorderTraversal = function(root) {
+  let stack = [], inorder = [];  
+  traverseLeft(root, stack);
+  while (stack.length) {
+    let node = stack.pop();
+    inorder.push(node.val);
+    if (node.right) {
+      traverseLeft(node.right, stack);
+    }
+  }
+  return inorder;
+};
+
+function traverseLeft(node, stack) {
+  while (node) {
+    stack.push(node);
+    node = node.left;
+  }
+}
