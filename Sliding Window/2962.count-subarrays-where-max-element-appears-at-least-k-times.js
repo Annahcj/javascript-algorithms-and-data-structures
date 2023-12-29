64343dfdfb774e21ec274bc056a2c159377dc9c5
Anna An <annaan.golf@gmail.com>
2023-12-29 22:42:52 +1300

    Add solution

--- Sliding Window/2962.count-subarrays-where-max-element-appears-at-least-k-times.js
+++ Sliding Window/2962.count-subarrays-where-max-element-appears-at-least-k-times.js
@@ -1,4 +1,4 @@
-// 10033. Count Subarrays Where Max Element Appears at Least K Times
+// 2962. Count Subarrays Where Max Element Appears at Least K Times
 // You are given an integer array nums and a positive integer k.
 // Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
 // A subarray is a contiguous sequence of elements within an array.
