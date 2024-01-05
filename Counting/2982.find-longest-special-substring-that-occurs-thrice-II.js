83d2a04b362bbd68522bb3e661c78f29546d9950
Anna An <annaan.golf@gmail.com>
2024-01-05 22:24:24 +1300

    Add solution

--- Counting/2982.find-longest-special-substring-that-occurs-thrice-II.js
+++ Counting/2982.find-longest-special-substring-that-occurs-thrice-II.js
@@ -5,7 +5,7 @@
 // A substring is a contiguous non-empty sequence of characters within a string.
 
 
-// Solution: Binary Search
+// Solution 1: Binary Search
 
 // Binary search for the longest equal substring which occurs at least three times.
 // For a length `len`, iterate through s and keep a running count of consectutive equal characters.
@@ -35,6 +35,45 @@ function occursThreeTimes(s, len) {
   }
   return false;
 }
+
+
+// Solution 2: Counting
+
+// Keep a running count of consecutive equal characters.
+// Each running count `count` at index i includes all substrings ending at index i of length `count`, `count - 1`, `count - 2`, ..., 1.
+// Keep track of the maximum three counts, and the minimum of those three counts will be our answer.
+  // e.g: maxThree = [3,3,2], ans -> 2
+  // Explanation: Each length 3 includes a substring of length 2.
+  // e.g: maxThree = [3,3,3], ans -> 3
+
+// Time Complexity: O(n) 132ms
+// Space Complexity: O(26 * 3) = O(1) 53.8MB
+var maximumLength = function(s) {
+  let n = s.length, maxThree = Array(26).fill(0).map(() => [0, 0, 0]);
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    if (i === 0 || s[i] !== s[i - 1]) count = 1;
+    else count++;
+    
+    let charcode = s.charCodeAt(i) - 97;
+    if (count >= maxThree[charcode][0]) {
+      maxThree[charcode][2] = maxThree[charcode][1];
+      maxThree[charcode][1] = maxThree[charcode][0];
+      maxThree[charcode][0] = count;
+    } else if (count >= maxThree[charcode][1]) {
+      maxThree[charcode][2] = maxThree[charcode][1];
+      maxThree[charcode][1] = count;
+    } else if (count > maxThree[charcode][2]) {
+      maxThree[charcode][2] = count;
+    }
+  }
+  
+  let maxLen = 0;
+  for (let i = 0; i < 26; i++) {
+    maxLen = Math.max(maxLen, maxThree[i][2]);
+  }
+  return maxLen === 0 ? -1 : maxLen;
+};
 
 // Three test cases
 console.log(maximumLength("aaaa")) // 2
