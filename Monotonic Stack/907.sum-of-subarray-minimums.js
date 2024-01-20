dd915b51db494f5893bce3dafbafc149cc2c9430
Anna An <annaan.golf@gmail.com>
2024-01-20 22:28:05 +1300

    Add solution

--- Monotonic Stack/907.sum-of-subarray-minimums.js
+++ Monotonic Stack/907.sum-of-subarray-minimums.js
@@ -6,44 +6,44 @@
 
 // For each number, find the previous smallest number and the next smallest number.
 // e.g: [2,5,3,8,1] for the number 3,
-//         |     |
-//        Prev  Next
+//       |       |
+//      Prev    Next
 // 2 is the previous number smaller than 3, and 1 is the next number smaller than 3.
 // The distance from 3 to 2 is 2, and the distance from 3 to 1 is 2.
 // We can get the number of subarrays which contain 3 by this formula: dist to prev smallest * dist to next smallest
-// Proof: [5,1,3,8,2] 2 * 2 = 4 subarrays.
+// Proof: [2,5,_3_,8,1] -> 2 * 2 = 4 subarrays.
 // start with 3: [3],[3,8]
 // start with 5: [5,3],[5,3,8]
 
-// 1. Keep a monotonic increasing stack initialized with [-1].
-// 2. Loop forwards in arr and populate the left array.
-// 3. Loop backwards in arr and populate the right array.
-// 4. Compute the answer: arr[i] * left[i] * right[i]: (number * number of subarrays it is in)
+// 1. Iterate from right to left and find the first number smaller than arr[i] on the right side.
+// 2. Iterate from left to right and find the first number smaller than or equal to arr[i] on the left side.
+  // Based on the indices of the left and right smaller numbers, we can find the numbers of subarrays based on the distance.
+  // Add arr[i] * subarrays to the total.
 
-// Time Complexity: O(n) 104ms
-// Space Complexity: O(n) 47.9MB
+// Time Complexity: O(n) 90ms
+// Space Complexity: O(n) 58.4MB
 var sumSubarrayMins = function(arr) {
-  let stack = [-1], n = arr.length;
-  let mod = 10 ** 9 + 7;
-  let left = Array(n), right = Array(n);
-  for (let i = 0; i < n; i++) {
-    while (stack[stack.length - 1] !== -1 && arr[stack[stack.length - 1]] >= arr[i]) {
-      stack.pop();
-    }
-    left[i] = i - stack[stack.length - 1];
-    stack.push(i);
-  }
-  stack = [n];
-  let ans = 0;
+  let n = arr.length, stack = [];
+  let smallerRight = Array(n).fill(n);
   for (let i = n - 1; i >= 0; i--) {
-    while (stack[stack.length - 1] !== n && arr[stack[stack.length - 1]] > arr[i]) {
-      stack.pop();
+    while (stack.length && arr[stack[stack.length - 1]] >= arr[i]) stack.pop();
+    if (stack.length) {
+      smallerRight[i] = stack[stack.length - 1];
     }
-    right[i] = stack[stack.length - 1] - i;
-    ans += arr[i] * left[i] * right[i];
     stack.push(i);
   }
-  return ans % mod;
+  stack = [];
+  let ans = 0, MOD = 1000000007;
+  for (let i = 0; i < n; i++) {
+    while (stack.length && arr[stack[stack.length - 1]] > arr[i]) stack.pop();
+    let smallerLeft = stack.length ? stack[stack.length - 1] : -1;
+    let lengthRight = smallerRight[i] - i;
+    let lengthLeft = i - smallerLeft;
+    let subarrays = lengthRight * lengthLeft;
+    ans = (ans + arr[i] * subarrays) % MOD;
+    stack.push(i);
+  }
+  return ans;
 };
 
 // Three test cases 
