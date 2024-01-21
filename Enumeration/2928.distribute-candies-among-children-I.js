eb2cc4e62dc1c22b602c5d389b0cb82873758fd0
Anna An <annaan.golf@gmail.com>
2024-01-21 18:48:38 +1300

    Add solution

---
+++ Enumeration/2928.distribute-candies-among-children-I.js
@@ -0,0 +1,31 @@
+// 2928. Distribute Candies Among Children I
+// You are given two positive integers n and limit.
+// Return the total number of ways to distribute n candies among 3 children such that no child gets more than limit candies.
+
+
+// Solution: Enumeration & Math
+
+// Iterate through each possible first amount, from min(n, limit) to max(0, n - limit * 2).
+// From the first amount, we can calculate the remaining amount: n - first amount.
+// From there, we can calculate the number of combinations,
+  // Calculate the maximum possible amount for the second or third element: max(limit, remaining).
+  // Calculate the minimum possible amount for the second or third element: remaining - max.
+  // The number of ways will be max - min + 1.
+
+// Time Complexity: O(limit) 50ms
+// Space Complexity: O(1) 49.8MB
+var distributeCandies = function(n, limit) {
+  let totalWays = 0; 
+  for (let firstAmount = Math.max(0, n - limit * 2); firstAmount <= Math.min(n, limit); firstAmount++) {
+    let remaining = n - firstAmount;
+    let max = Math.min(limit, remaining);
+    let min = remaining - max;
+    let ways = max - min + 1;
+    totalWays += ways;
+  }
+  return totalWays;
+};
+
+// Two test cases
+console.log(distributeCandies(5, 2)) // 3
+console.log(distributeCandies(3, 3)) // 10
