d1391a99e47de75d9bbb6a907b250fa985b73f32
Anna An <annaan.golf@gmail.com>
2024-01-25 21:54:35 +1300

    Add solution

--- Dynamic Programming/1143.longest-common-subsequence.js
+++ Dynamic Programming/1143.longest-common-subsequence.js
@@ -1,82 +1,58 @@
 // 1143. Longest Common Subsequence
 
 
-// Solution 1: Recursion w/ Memoization
+// Solution 1: DP - Recursion w/ Memoization
 
-// Use a 2d array to store the longest subsequences at each position
-// Call recurse(0, 0)
-// recurse: (i (index in text1), j (index in text2))
-  // base case: if i has gone out of bounds OR j has gone out of bounds, return 0
-  // base case2: if memo already contains [i, j], return memo[i][j]
-  // let ans be 0
-  // if text1[i] matches text2[j]
-    // (move both pointers forward one since they both match)
-    // set ans to 1 + recurse(i + 1, j + 1)
-  // otherwise,
-    // (try moving either one forward)
-    // set ans to Math.max(ans, recurse(i + 1, j), recurse(i, j + 1))
-  // store the best result (ans) in memo
-  // return ans
+// Memoize each dp(i, j), where
+  // i = index in text1
+  // j = index in text2
 
-// Time Complexity: O(nm) 132ms
-// Space Complexity: O(mn) 52.5MB
+// For each dp(i, j),
+  // If text1[i] is equal to text2[j], it's optimal to take the match and move both pointers up: 1 + dp(i + 1, j + 1).
+  // Otherwise, take the best result out of skipping either text1[i] or text2[j].
+
+// n = length of text1, m = length of text2
+// Time Complexity: O(nm) 125ms
+// Space Complexity: O(nm) 75.8MB
 var longestCommonSubsequence = function(text1, text2) {
   let n = text1.length, m = text2.length;
-  let memo = Array(n);
-  for (let i = 0; i < n; i++) {
-    memo[i] = Array(m);
-  }
-  return recurse(0, 0);
-
-  function recurse(i, j) {
-    if (i === text1.length || j === text2.length) return 0;
-    if (memo[i][j]) return memo[i][j];
+  let memo = Array(n).fill(0).map(() => Array(m).fill(-1));
+  return dp(0, 0);
+  
+  function dp(i, j) {
+    if (i === n || j === m) return 0;
+    if (memo[i][j] !== -1) return memo[i][j];
+    
     let ans = 0;
     if (text1[i] === text2[j]) {
-      ans = 1 + recurse(i + 1, j + 1);
+      ans = Math.max(ans, 1 + dp(i + 1, j + 1));
     } else {
-      ans = Math.max(ans, recurse(i + 1, j), recurse(i, j + 1));
+      ans = Math.max(dp(i + 1, j), dp(i, j + 1));
     }
-    memo[i][j] = ans;
-    return ans;
-  }  
+    return memo[i][j] = ans;
+  }
 };
 
-// Solution 2: Dynamic Programming
 
-// Instead of using recursion, we can use bottom-up dynamic programming.
-// Create an n + 1 by m + 1 matrix filled with 0's
-// Loop through backwards from n - 1 to 0 (pointer = i)
-  // loop through backwards from m - 1 to 0 (pointer = j)
-    // set ans to 0 
-    // if text1[i] matches text2[j]
-      // set ans to 1 + dp[i + 1][j + 1]
-    // otherwise,
-      // set ans to Math.max(dp[i + 1][j], dp[i][j + 1])
-    // set dp[i][j] to ans
-// return dp[0][0]
+// Solution 2: Bottom-Up Iterative DP
 
-// Time Complexity: O(nm) 136ms
-// Space Complexity: O(nm) 48.4MB
+// The same approach as solution 1, but the iterative version.
+
+// Time Complexity: O(nm) 125ms
+// Space Complexity: O(nm) 77.6MB
 var longestCommonSubsequence = function(text1, text2) {
   let n = text1.length, m = text2.length;
-  let dp = Array(n + 1);
-  for (let i = 0; i <= n; i++) {
-    dp[i] = Array(m + 1).fill(0);
-  }
-
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = m - 1; j >= 0; j--) {
-      let ans = 0;
+  let dp = Array(n + 1).fill(0).map(() => Array(m + 1).fill(0));
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
       if (text1[i] === text2[j]) {
-        ans = 1 + dp[i + 1][j + 1];
+        dp[i + 1][j + 1] = 1 + dp[i][j];
       } else {
-        ans = Math.max(dp[i + 1][j], dp[i][j + 1]);
+        dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
       }
-      dp[i][j] = ans;
     }
   }
-  return dp[0][0];
+  return dp[n][m];
 };
 
 // Three test cases
