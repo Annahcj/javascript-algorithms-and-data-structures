4324e3123d7735638905358de319ab987cf1f8cd
Anna An <annaan.golf@gmail.com>
2024-01-30 22:55:19 +1300

    Add solution

--- Math/3000.maximum-area-of-longest-diagonal-rectangle.js
+++ Math/3000.maximum-area-of-longest-diagonal-rectangle.js
@@ -1,4 +1,4 @@
-// 10035. Maximum Area of Longest Diagonal Rectangle
+// 3000. Maximum Area of Longest Diagonal Rectangle
 // You are given a 2D 0-indexed integer array dimensions.
 // For all indices i, 0 <= i < dimensions.length, dimensions[i][0] represents the length and dimensions[i][1] represents the width of the rectangle i.
 // Return the area of the rectangle having the longest diagonal. If there are multiple rectangles with the longest diagonal, return the area of the rectangle having the maximum area.
