da49e03e68ac2bf36a068ae9ee6a3cf6557e035f
Anna An <annaan.golf@gmail.com>
2024-02-13 20:48:07 +1300

    Add solution

--- Two Pointers/2108.find-first-palindromic-string-in-the-array.js
+++ Two Pointers/2108.find-first-palindromic-string-in-the-array.js
@@ -4,27 +4,27 @@
 
 // Solution: Two Pointers
 
-// Use two pointers to check if a word is a palindrome.
-// Return the first word that is a palindrome, otherwise return "".
+// Iterate through the words to find the first palindrome.
+// To check whether a word is a palindrome, use two pointers starting from index (0, n - 1) and incrementally work your way to the middle checking whether each pair of characters is equal.
 
-// n = length of words, m = length of longest word in words
-// Time Complexity: O(nm)
-// Space Complexity: O(1)
+// n = length of words, m = words[i].length
+// Time Complexity: O(nm) 54ms
+// Space Complexity: O(1) 53MB
 var firstPalindrome = function(words) {
   for (let word of words) {
-    if (isPalin(word)) return word;
-  }
-  return "";
-  
-  function isPalin(str) {
-    let start = 0, end = str.length - 1;
-    while (start < end) {
-      if (str[start] !== str[end]) return false;
-      start++, end--;
-    }
-    return true;
+    if (isPalindrome(word)) return word;
   }  
+  return "";
 };
+
+function isPalindrome(word) {
+  let start = 0, end = word.length - 1;
+  while (start < end) {
+    if (word[start] !== word[end]) return false;
+    start++, end--;
+  }
+  return true;
+}
 
 // Two test cases
 console.log(firstPalindrome(["abc","car","ada","racecar","cool"])) // "ada"
