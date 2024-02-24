edb9ad20d5842ae4bf9886d61eb8083e6b01579d
Anna An <annaan.golf@gmail.com>
2024-02-24 22:55:32 +1300

    Add solution

---
+++ AVL Tree/1606.find-servers-that-handled-most-number-of-requests.js
@@ -0,0 +1,301 @@
+// 1606. Find Servers That Handled Most Number of Requests
+// You have k servers numbered from 0 to k-1 that are being used to handle multiple requests simultaneously. Each server has infinite computational capacity but cannot handle more than one request at a time. The requests are assigned to servers according to a specific algorithm:
+  // The ith (0-indexed) request arrives.
+  // If all servers are busy, the request is dropped (not handled at all).
+  // If the (i % k)th server is available, assign the request to that server.
+  // Otherwise, assign the request to the next available server (wrapping around the list of servers and starting from 0 if necessary). For example, if the ith server is busy, try to assign the request to the (i+1)th server, then the (i+2)th server, and so on.
+// You are given a strictly increasing array arrival of positive integers, where arrival[i] represents the arrival time of the ith request, and another array load, where load[i] represents the load of the ith request (the time it takes to complete). Your goal is to find the busiest server(s). A server is considered busiest if it handled the most number of requests successfully among all the servers.
+// Return a list containing the IDs (0-indexed) of the busiest server(s). You may return the IDs in any order.
+
+
+// Solution: AVL Tree & Heap
+
+// Use a min heap to keep track of currently occupied servers.
+// Use an AVL tree to store available servers and to binary search for the smallest serverId >= (i % k).
+
+// For each request, remove servers from the occupied heap that are no longer occupied (nextAvailableTime <= arrival time) and add them into the AVL tree.
+// Binary search for the smallest serverId in the avlTree where serverId >= i % k.
+// If there is no such serverId >= i % k, search for the smallest serverId.
+// After finding the server, 
+  // Update the number of calls for the serverId.
+  // Remove the serverId from the avlTree.
+  // Add the serverId and next available time to the min heap.
+
+// n = number of requests, k = number of servers
+// Time Complexity: O(n log(k)) 391ms
+// Space Complexity: O(k) 83.4MB
+var busiestServers = function(k, arrival, load) {
+  let avlTree = new AVLTree(), busy = new Heap((a, b) => a[1] - b[1]); // [serverId, nextAvailableTime]
+  for (let i = 0; i < k; i++) {
+    avlTree.insert(i);
+  }
+  let serverCalls = Array(k).fill(0), n = arrival.length;
+  for (let i = 0; i < n; i++) {
+    while (!busy.isEmpty() && busy.top()[1] <= arrival[i]) {
+      let [serverId] = busy.remove();
+      avlTree.insert(serverId);
+    }
+    let idealIndex = i % k;
+    // Search for the smallest serverId in avlTree where serverId >= idealIndex.
+    let lowerBound = avlTree.lowerBound(idealIndex);
+    if (lowerBound) {
+      serverCalls[lowerBound.val]++;
+      busy.add([lowerBound.val, arrival[i] + load[i]]);
+      avlTree.remove(lowerBound.val);
+      continue;
+    }
+    // If there is no larger or equal serverId, search for the smallest serverId.
+    let smallestAvailableServer = avlTree.getKthSmallest(1);
+    if (smallestAvailableServer) {
+      serverCalls[smallestAvailableServer.val]++;
+      busy.add([smallestAvailableServer.val, arrival[i] + load[i]]);
+      avlTree.remove(smallestAvailableServer.val);
+      continue;
+    }
+  }
+  let busiestServers = [], maxCalls = 0;
+  for (let i = 0; i < k; i++) {
+    if (serverCalls[i] > maxCalls) {
+      busiestServers = [i];
+      maxCalls = serverCalls[i];
+    } else if (serverCalls[i] === maxCalls) {
+      busiestServers.push(i);
+    }
+  }
+  return busiestServers;
+};
+
+class AVLTreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.height = 1;
+    this.size = 1; // number of nodes in tree rooted at this node
+  }
+}
+
+class AVLTree {
+  constructor() {
+    this.root = null;
+  }
+  search(val, node = this.root) {
+    if (!node) return null;
+    if (node.val === val) return node;
+    if (val < node.val) {
+      return this.search(val, node.left);
+    } else {
+      return this.search(val, node.right);
+    }
+  }
+  insert(val) {
+    return this.root = this._insert(val, this.root);
+  }
+  _insert(val, node) {
+    if (!node) return new AVLTreeNode(val);
+    if (val < node.val) {
+      node.left = this._insert(val, node.left);
+    } else {
+      node.right = this._insert(val, node.right);
+    }
+    node.height = 1 + Math.max(this._getHeight(node.left), this._getHeight(node.right));
+    node.size = 1 + this._getSize(node.left) + this._getSize(node.right);
+
+    return this._rebalance(node);
+  }
+  remove(val, node = this.root) {
+    // To ensure we don't delete all occurances of `val`, pass in the reference of one occurance.
+    // To delete all nodes with value `val`, remove the check for `nodeToRemove` in _remove.
+    const nodeToRemove = this.search(val, node);
+    return this.root = this._remove(val, nodeToRemove, node);
+  }
+  // Four scenarios for deletion:
+    // 1. node to delete has no children - just delete it
+    // 2. node to delete only has a left child - replace it with the left child
+    // 3. node to delete only has a right child - replace it with the right child
+    // 4. node to delete has both left and right children - replace it with the next smallest node that is larger (use in order traversal to find the leftmost node in the right child)
+  _remove(val, nodeToRemove, node) {
+    if (!node) return null;
+    if (val < node.val) {
+      node.left = this._remove(val, nodeToRemove, node.left);
+    } else if (val > node.val) {
+      node.right = this._remove(val, nodeToRemove, node.right);
+    } else if (val === node.val && node === nodeToRemove) {
+      if (!node.left && !node.right) return null;
+      if (!node.right) return node.left;
+      if (!node.left) return node.right;
+
+      // has both left and right children
+      // inorder traversal on the right child to get the leftmost node
+      // replace the node value with the leftmost node value and remove the leftmost node from the right subtree
+      const leftmostNode = this._getLeftmost(node.right);
+      node.val = leftmostNode.val;
+
+      node.right = this._remove(leftmostNode.val, this.search(leftmostNode.val, node.right), node.right);
+    } else {
+      return node;
+    }
+    node.height = 1 + Math.max(this._getHeight(node.left), this._getHeight(node.right));
+    node.size = 1 + this._getSize(node.left) + this._getSize(node.right);
+
+    return this._rebalance(node);
+  }
+  getKthLargest(k) { // 1-indexed
+    let node = this.root;
+    if (!node || node.size < k) return null; // there is no kth element
+
+    while (node) {
+      let rightSize = node.right?.size ?? 0;
+      if (k === rightSize + 1) return node;
+      if (rightSize >= k) {
+        node = node.right;
+      } else {
+        k -= rightSize + 1;
+        node = node.left;
+      }
+    }
+    return null;
+  }
+  getKthSmallest(k) { // 1-indexed
+    let node = this.root;
+    if (!node || node.size < k) return null; // there is no kth element
+
+    while (node) {
+      let leftSize = node.left?.size ?? 0;
+      if (k === leftSize + 1) return node;
+      if (leftSize >= k) {
+        node = node.left;
+      } else {
+        k -= leftSize + 1;
+        node = node.right;
+      }
+    }
+    return null;
+  }
+  lowerBound(val, node = this.root) { // find node with the smallest value, where the value >= val
+    if (!node) return null;
+    if (node.val === val) return node;
+    if (val < node.val) {
+      const res = this.lowerBound(val, node.left);
+      return res ? res : node;
+    } else {
+      return this.lowerBound(val, node.right);
+    }
+  }
+  _getLeftmost(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+  _getHeight(node = this.root) {
+    return node ? node.height : 0;
+  }
+  _getSize(node = this.root) {
+    return node ? node.size : 0;
+  }
+  _getBalance(node = this.root) {
+    return node ? this._getHeight(node.left) - this._getHeight(node.right) : 0;
+  }
+  _leftRotation(node) { 
+    let rightNode = node.right;
+    let rightNodeLeftChild = rightNode.left;
+    rightNode.left = node;
+    node.right = rightNodeLeftChild;
+
+    // node is now below rightNode and needs to be updated first
+    node.height = 1 + Math.max(this._getHeight(node.left), this._getHeight(node.right));
+    rightNode.height = 1 + Math.max(this._getHeight(rightNode.left), this._getHeight(rightNode.right));
+
+    node.size = 1 + this._getSize(node.left) + this._getSize(node.right);
+    rightNode.size = 1 + this._getSize(rightNode.left) + this._getSize(rightNode.right);
+
+    return rightNode; // right node is the new root
+  }
+  _rightRotation(node) {
+    let leftNode = node.left;
+    let leftNodeRightChild = leftNode.right;
+    leftNode.right = node;
+    node.left = leftNodeRightChild;
+
+    // node is now below leftNode and needs to be updated first
+    node.height = 1 + Math.max(this._getHeight(node.left), this._getHeight(node.right));
+    leftNode.height = 1 + Math.max(this._getHeight(leftNode.left), this._getHeight(leftNode.right));
+
+    node.size = 1 + this._getSize(node.left) + this._getSize(node.right);
+    leftNode.size = 1 + this._getSize(leftNode.left) + this._getSize(leftNode.right);
+
+    return leftNode; // left node is the new root
+  }
+  _rebalance(node) {
+    const balance = this._getBalance(node);
+    if (balance > 1 && this._getBalance(node.left) >= 0) { // left left
+      return this._rightRotation(node);
+    } else if (balance > 1 && this._getBalance(node.left) < 0) { // left right
+      node.left = this._leftRotation(node.left);
+      return this._rightRotation(node);
+    } else if (balance < -1 && this._getBalance(node.right) <= 0) { // right right
+      return this._leftRotation(node);
+    } else if (balance < -1 && this._getBalance(node.right) > 0) { // right left
+      node.right = this._rightRotation(node.right);
+      return this._leftRotation(node);
+    }
+    return node;
+  }
+}
+
+class Heap {
+  constructor(comparator = ((a, b) => a - b)) {
+    this.values = [];
+    this.comparator = comparator;
+    this.size = 0;
+  }
+  add(val) {
+    this.size++;
+    this.values.push(val);
+    let idx = this.size - 1, parentIdx = Math.floor((idx - 1) / 2);
+    while (parentIdx >= 0 && this.comparator(this.values[parentIdx], this.values[idx]) > 0) {
+      [this.values[parentIdx], this.values[idx]] = [this.values[idx], this.values[parentIdx]];
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+  }
+  remove() {
+    if (this.size === 0) return -1;
+    this.size--;
+    if (this.size === 0) return this.values.pop();
+    let removedVal = this.values[0];
+    this.values[0] = this.values.pop();
+    let idx = 0;
+    while (idx < this.size && idx < Math.floor(this.size / 2)) {
+      let leftIdx = idx * 2 + 1, rightIdx = idx * 2 + 2;
+      if (rightIdx === this.size) {
+        if (this.comparator(this.values[leftIdx], this.values[idx]) > 0) break;
+        [this.values[leftIdx], this.values[idx]] = [this.values[idx], this.values[leftIdx]];
+        idx = leftIdx;
+      } else if (this.comparator(this.values[leftIdx], this.values[idx]) < 0 || this.comparator(this.values[rightIdx], this.values[idx]) < 0) {
+        if (this.comparator(this.values[leftIdx], this.values[rightIdx]) <= 0) {
+          [this.values[leftIdx], this.values[idx]] = [this.values[idx], this.values[leftIdx]];
+          idx = leftIdx;
+        } else {
+          [this.values[rightIdx], this.values[idx]] = [this.values[idx], this.values[rightIdx]];
+          idx = rightIdx;
+        }
+      } else {
+        break;
+      }
+    }
+    return removedVal;
+  }
+  top() {
+    return this.values[0];
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+// Three test cases
+console.log(busiestServers(3, [1,2,3,4,5], [5,2,3,3,3] )) // [1]
+console.log(busiestServers(3, [1,2,3,4], [1,2,1,2])) // [0]
+console.log(busiestServers(3, [1,2,3], [10,12,11])) // [0,1,2]
