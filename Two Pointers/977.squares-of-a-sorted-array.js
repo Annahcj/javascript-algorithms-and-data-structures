d39ef35ff3a7b102ad0fa0387bc40a0c16c4f975
Anna An <annaan.golf@gmail.com>
2024-03-02 16:23:33 +1300

    Add solution

--- Two Pointers/977.squares-of-a-sorted-array.js
+++ Two Pointers/977.squares-of-a-sorted-array.js
@@ -4,32 +4,25 @@
 
 // Solution: Two Pointers
 
-// Set two pointers on both ends of nums.
-// Loop through nums from back to front (pointer = i)
-  // If the absolute value of nums[start] is bigger than the absolute value of nums[end]
-    // set result[i] to the square of nums[start]
-    // increment start
-  // Otherwise,
-    // set result[i] to the square of nums[end]
-    // decrement end
-// Return result
+// The square of negative numbers becomes positive, so only the absolute value matters.
+// To populate the squares in descending order, 
+// Use two pointers at the start and end of nums and take the maximum absolute value out of the two at each step.
 
-// Time Complexity: O(n) 104ms
-// Space Complexity: O(1) (not including the output array) 46.1MB
+// Time Complexity: O(n) 92ms
+// Space Complexity: O(1) (excluding output) 55.4MB
 var sortedSquares = function(nums) {
-  let n = nums.length;
-  let res = Array(n);
+  let n = nums.length, squares = Array(n);
   let start = 0, end = n - 1;
   for (let i = n - 1; i >= 0; i--) {
-    if (Math.abs(nums[start]) > Math.abs(nums[end])) { // nums[start] is a bigger square than nums[end]
-      res[i] = nums[start] * nums[start];
+    if (Math.abs(nums[start]) >= Math.abs(nums[end])) {
+      squares[i] = nums[start] * nums[start];
       start++;
-    } else { 
-      res[i] = nums[end] * nums[end];
+    } else {
+      squares[i] = nums[end] * nums[end];
       end--;
     }
   }
-  return res;
+  return squares;
 };
 
 // Two test cases 
