00c580643ff8a19326876544e5ca456624b90ce2
Anna An <annaan.golf@gmail.com>
2024-03-14 22:55:15 +1300

    Add solution

--- Sliding Window/930.binary-subarrays-with-sum.js
+++ Sliding Window/930.binary-subarrays-with-sum.js
@@ -3,64 +3,52 @@
 // A subarray is a contiguous part of the array.
 
  
-// Solution 1: Prefix Sum w/ Hashmap
+// Solution 1: Hashmap & Prefix Sum
 
-// Use a hashmap 'count' to store the number of times a prefix sum occurs in nums
-// Initially, set count[0] to 1.
-// Loop through nums (pointer = i)
-  // add nums[i] to prefixSum
-  // add count[prefixSum - goal] to ans
-  // increment count of prefixSum
-// Return ans
+// Two sum approach.
+// Store occurances of each prefix sum in a hashmap
+// Find the number of previous occurances of the current prefix sum - goal, as it means the subarray in between the current and previous index has a sum equal to goal.
 
-// Time Complexity: O(n) 92ms
-// Space Complexity: O(n) 44.1MB
+// Time Complexity: O(n) 66ms
+// Space Complexity: O(n) 54.2MB
 var numSubarraysWithSum = function(nums, goal) {
-  let count = {0: 1}, ans = 0, prefixSum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    prefixSum += nums[i];
-    ans += (count[prefixSum - goal] || 0);
-    count[prefixSum] = (count[prefixSum] || 0) + 1;
+  let map = {}, sum = 0, subarrays = 0;
+  map[0] = 1;
+  for (let num of nums) {
+    sum += num;
+    subarrays += (map[sum - goal] || 0);
+    map[sum] = (map[sum] || 0) + 1;
   }
-  return ans;
+  return subarrays;
 };
 
 
 // Solution 2: Sliding Window
 
-// function atMost returns the number of subarrays with sums smaller than or equal to goal
-// returning atMost(goal) - atMost(goal - 1) returns the number of subarrays with sums exactly equal to goal
+// Count all subarrays where the sum is less than or equal to goal.
+// Count all subarrays where the sum is less than or equal to goal - 1.
+// Maintain a sliding window of maximum length, where the sum doesn't exceed a certain sum.
 
-// atMost: (goal)
-  // if goal is smaller than 0, return 0 (the sum can never be smaller than 0 since we only have 1's and 0's)
-  // set ans to 0, left to 0
-  // loop through nums (pointer = right)
-    // decrement goal by nums[right]
-    // loop while goal is smaller than 0,
-      // increment goal by nums[left]
-      // increment left by one
-    // add right - left + 1 to ans
-  // return ans
+// Return the (subarrays with sum <= goal) - (subarrays with sum <= goal - 1).
 
-// Time Complexity: O(n) 76ms
-// Space Complexity: O(1) 42.1MB
+// Time Complexity: O(n) 72ms
+// Space Complexity: O(1) 50.7MB
 var numSubarraysWithSum = function(nums, goal) {
-  function atMost(goal) {
-    if (goal < 0) return 0;
-    let ans = 0;
-    let left = 0;
-    for (let right = 0; right < nums.length; right++) {
-      goal -= nums[right];
-      while (goal < 0) {
-        goal += nums[left];
-        left++;
-      }
-      ans += (right - left) + 1;
-    }
-    return ans;
-  }
-  return atMost(goal) - atMost(goal - 1);
+  return countSubarraysWithMaxSum(nums, goal) - countSubarraysWithMaxSum(nums, goal - 1);
 };
+
+function countSubarraysWithMaxSum(nums, maxSum) {
+  let sum = 0, subarrays = 0;
+  for (let j = 0, i = 0; j < nums.length; j++) {
+    sum += nums[j];
+    while (i <= j && sum > maxSum) {
+      sum -= nums[i];
+      i++;
+    }
+    subarrays += (j - i + 1);
+  }
+  return subarrays;
+}
 
 // Four test cases 
 console.log(numSubarraysWithSum([1,0,1], 2)) // 1
