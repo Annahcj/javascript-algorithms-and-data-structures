8bb2f57cb2e2825bd1f4d2b4499f2ad95c2c0757
Anna An <annaan.golf@gmail.com>
2024-03-16 21:46:51 +1300

    Add solution

--- Prefix Sum/525.contiguous-array.js
+++ Prefix Sum/525.contiguous-array.js
@@ -2,37 +2,31 @@
 // Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
 
 
-// Solution: Prefix Sum & Hashmap
+// Solution: Prefix Diff & Hashmap
 
-// Loop through nums and count the number of zeros and ones there are so far.
+// Go through nums and count the number of zeros and ones there are so far.
   // Get the difference: zeros count - ones count.
-  // If the hashmap already contains the diff, that means the amount of zeros and ones have increased by the same amount. 
-    // i - map.get(diff) = length of the subarray.
+  // If the hashmap already contains the same diff, that means the amount of zeros and ones have increased by the same amount. 
+    // The length of the subarray is i - map[diff].
     // Proof: Let's imagine 4 steps ago, there were 2 zeros and 1 one. If we now have 4 zeros and 3 ones, that means we have increased zeros and ones by the same amount.
   // Otherwise, add the diff with the current index into the map.
 
-// Time complexity: O(n) 139ms
-// Space Complexity: O(n) 51.6MB
+// Time Complexity: O(n) 106ms
+// Space Complexity: O(n) 58.9MB
 var findMaxLength = function(nums) {
-  let map = new Map();
-  map.set(0, -1); // for equal counts
-  
-  let zeros = 0, ones = 0;
-  let ans = 0;
-  for (var i = 0; i < nums.length; i++) {
-    if (nums[i] === 0) zeros++;
-    else ones++;
-    
-    let key = zeros - ones;
-    if (map.has(key)) {
-      ans = Math.max(ans, i - map.get(key));
+  let n = nums.length, map = {0: -1};
+  let diff = 0, maxLen = 0;
+  for (let i = 0; i < n; i++) {
+    diff += nums[i] === 1 ? 1 : -1;
+    if (map[diff] !== undefined) {
+      maxLen = Math.max(maxLen, i - map[diff]);
     } else {
-      map.set(key, i);
+      map[diff] = i;
     }
   }
-  return ans;
+  return maxLen;
 };
 
-// Two test cases to run function on
+// Two test cases
 console.log(findMaxLength([0, 1])) // 2
 console.log(findMaxLength([0,1,0])) // 2
