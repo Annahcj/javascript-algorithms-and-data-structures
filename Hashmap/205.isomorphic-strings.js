8d5d4cd029d3fe701dc03afa5793e8e75c85883c
Anna An <annaan.golf@gmail.com>
2024-04-02 22:39:03 +1300

    Add solution

--- Hashmap/205.isomorphic-strings.js
+++ Hashmap/205.isomorphic-strings.js
@@ -2,35 +2,26 @@
 // Given two strings s and t, determine if they are isomorphic.
 
 
-// Solution 1: Two Hash Maps
+// Solution: Hashmap
 
-// Since the length of s and t are guaranteed to be equal, we can use one for-loop to traverse both strings.
-// For each character in s and t, check if they are already mapped, map them if they aren't, or return false if they are different from their mapped values.
+// Maintain a two-way mapping between s[i] -> t[i] and t[i] -> s[i].
+// If at any point, stMap[s[i]] is not equal to t[i] or tsMap[t[i]] is not equal to s[i].
 
-// e.g: 'foo' & 'bar' => 
-// loop: 
-// i = 0, first map doesn't contain 'f' and second map doesn't contain 'b', so we add them in.
-// i = 1, first map doesn't contain 'o' and second map doesn't contain 'a', so we add them in.
-// i = 2, first map contains 'o' but it's value ('a') is not equal to 'r', so we return false.
-
-// Time Complexity: O(n) 112ms
-// Space Complexity: O(1) 41.7MB (there are maximum 26 characters in each object)
-
+// Time Complexity: O(n) 63ms
+// Space Complexity: O(n) 50.8MB
 var isIsomorphic = function(s, t) {
-    let charsS = {}, charsT = {};
-    for (var i = 0; i < s.length; i++) {
-      if (!charsS[s[i]] && !charsT[t[i]]) {
-        charsS[s[i]] = t[i];
-        charsT[t[i]] = s[i];
-      } else if (charsS[s[i]] !== t[i] || charsT[t[i]] !== s[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
+  let stMap = {}, tsMap = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!stMap[s[i]]) stMap[s[i]] = t[i];
+    if (!tsMap[t[i]]) tsMap[t[i]] = s[i];
+    // two-way mapping
+    if (stMap[s[i]] !== t[i] || tsMap[t[i]] !== s[i]) return false;
+  }
+  return true;
+};
   
-  // Four test cases to run function on
-  console.log(isIsomorphic("egg", "add")) // true
-  console.log(isIsomorphic("foo", "bar")) // false
-  console.log(isIsomorphic("paper", "title")) // true
-  console.log(isIsomorphic("badc", "baba")) // false
+// Four test cases
+console.log(isIsomorphic("egg", "add")) // true
+console.log(isIsomorphic("foo", "bar")) // false
+console.log(isIsomorphic("paper", "title")) // true
+console.log(isIsomorphic("badc", "baba")) // false
