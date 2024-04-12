b9a02316d272cd40d073c373684b82f185f512ac
Anna An <annaan.golf@gmail.com>
2024-04-12 21:18:08 +1200

    Add solution

--- Dynamic Programming/42.trapping-rain-water.js
+++ Dynamic Programming/42.trapping-rain-water.js
@@ -1,44 +1,28 @@
 // 42. Trapping Rain Water
 // Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
-// Solution: Dynamic Programming
 
-// Thoughts:
-// Use an array to store the lower wall of (the highest wall on the left and the highest wall on the right) at each given index.
+// Solution: DP
 
-// Algorithm:
-// Keep an array walls in which we will store the wall height
-// Loop through height from left to right, keeping a max left wall (highest wall on the left)
-  // Push max into 'walls'
-  // If height[i] is bigger than max, update max to be height[i].
-// Loop through height from right to left, keeping a max right wall (highest wall on the right)
-  // If right wall max is smaller than left wall max at index, update it. (we keep the smaller wall since the water will flow off)
-  // Update right wall max if necessary
-// Loop through walls (pointer = h), 
-  // if walls[h] - height[h] is bigger than zero, add it to total.
-// Return total.
+// The amount of water we can trap at position i is determined by the minimum out of the highest wall on the left and highest wall on the right of index i.
+// Use two passes to find the maximum wall height on the right and left of each position i.
 
-// Time Complexity: O(n) 72ms
-// Space Complexity: O(n) 40.8MB
-  var trap = function(height) {
-    let walls = [], total = 0;
-    let max = 0;
-    for (var i = 0; i < height.length; i++) {
-      walls.push(max);
-      max = Math.max(max, height[i]);
-    }
-    max = 0;
-    for (var j = height.length - 1; j >= 0; j--) {
-      walls[j] = Math.min(walls[j], max);
-      max = Math.max(max, height[j]);
-    }
-    for (var h = 0; h < height.length; h++) {
-      let water = walls[h] - height[h];
-      if (water > 0) total += water;
-    }
-    return total
-  };
+// Time Complexity: O(n) 66ms
+// Space Complexity: O(n) 52.4MB
+var trap = function(height) {
+  let n = height.length, maxRight = Array(n).fill(0);   
+  for (let i = n - 2; i >= 0; i--) {
+    maxRight[i] = Math.max(maxRight[i + 1], height[i + 1]);
+  }
+  let maxLeft = height[0], sum = 0;
+  for (let i = 1; i < n; i++) {
+    let minHeight = Math.min(maxLeft, maxRight[i]);
+    sum += Math.max(0, minHeight - height[i]);
+    maxLeft = Math.max(maxLeft, height[i]);
+  }
+  return sum;
+};
   
-  // Two test cases to run function on
-  console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])) // 6
-  console.log(trap([4,2,0,3,2,5])) // 9
+// Two test cases
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])) // 6
+console.log(trap([4,2,0,3,2,5])) // 9
