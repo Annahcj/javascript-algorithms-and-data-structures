7b81a42954d3b80525307fab15398995dab78397
Anna An <annaan.golf@gmail.com>
2024-04-18 22:53:02 +1200

    Add solution

--- Matrix /463.island-perimeter.js
+++ Matrix /463.island-perimeter.js
@@ -4,38 +4,32 @@
 // The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 
 
-// Solution: Count Neighboring Land Cells
+// Solution: Counting
 
-// For each cell in grid, 
-// add 4 to the total perimeter,
-// then check each four neighboring cells, subtract the number of neighboring land cells from total perimeter.
+// For each land cell, go through each four directions (top, left, right, bottom), and count the number of sides connected to either water or the edge of the grid.
+// Get the total sum of these water/edge sides across all land cells.
 
-// return perimeter.
-
-// Time Complexity: O(nm) 303ms
-// Space Complexity: O(1) 48.9MB
+// Time Complexity: O(4mn) 97ms
+// Space Complexity: O(1) 59.3MB
 var islandPerimeter = function(grid) {
+  let m = grid.length, n = grid[0].length;
+  const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
   let perimeter = 0;
-  let n = grid.length, m = grid[0].length;
-  const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-  for (var i = 0; i < n; i++) {
-    for (var j = 0; j < m; j++) {
-      if (grid[i][j] === 1) {
-        perimeter += 4;
-        for (var [x, y] of directions) {
-          let newX = i + x, newY = j + y;
-          if (newX < 0 || newX >= n || newY < 0 || newY >= m) continue;
-          if (grid[newX][newY] === 1) {
-            perimeter--;
-          }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 0) continue;
+      for (let [x, y] of directions) {
+        let neiRow = i + x, neiCol = j + y;
+        if (neiRow < 0 || neiRow >= m || neiCol < 0 || neiCol >= n || grid[neiRow][neiCol] === 0) {
+          perimeter++;
         }
       }
     }
-  }  
+  }
   return perimeter;
 };
 
-// Three test cases to run function on
+// Three test cases
 console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]])) // 16
 console.log(islandPerimeter([[1]])) // 4
 console.log(islandPerimeter([[1,0]])) // 4
