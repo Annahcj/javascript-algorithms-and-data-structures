28c5a6a181d66257be9eb8c6e403d63a22ada406
Anna An <annaan.golf@gmail.com>
2024-04-27 21:09:51 +1200

    Add solution

--- Dynamic Programming/514.freedom-trail.js
+++ Dynamic Programming/514.freedom-trail.js
@@ -7,46 +7,84 @@
   // 2. If the character key[i] has been aligned at the "12:00" direction, press the center button to spell, which also counts as one step. After the pressing, you could begin to spell the next character in the key (next stage). Otherwise, you have finished all the spelling.
 
 
-// Solution: DP - Recursion w/ Memoization
+// Solution 1: DP - Recursion w/ Memoization
 
-// Memoize each dp(i, j), where i = index in key and j = index in ring.
-// For each key[i], try going to each of the indexes of it and record the best result.
+// Map the positions of occurances to each character: {g: [0,3,4], d: [1,2], ...}.
 
-// Since there are two ways to calculate the cost to reach an index (clockwise and anti-clockwise), it can be calculated as such (when i <= j):
-  // Anti-Clockwise: j - i
-  // Clockwise: i + ring.length - j
-// Get the minimum out of the two directions.
+// Memoize each dp(ringIndex, keyIndex).
+// For each key character, try all positions of that character.
+// There are two directions to get to a new position (clockwise or anticlockwise). Since it doesn't matter "how" we get there, take the direction with the minimum number of moves.
+// Record and return the minimum number of moves to complete all characters in the key.
 
-// n = length of key, m = length of ring
-// Time Complexity: O(nm^2) 137ms
-// Space Complexity: O(nm) 49.5MB
+// n = length of ring, m = length of key
+// Time Complexity: O(n^2 * m) 87ms
+// Space Complexity: O(nm) 58MB
 var findRotateSteps = function(ring, key) {
-  let m = ring.length, n = key.length;
-  let indices = Array(26).fill(0).map(() => []);
-  for (let i = 0; i < m; i++) {
-    indices[ring.charCodeAt(i) - 97].push(i);
+  let positionsMap = {};
+  for (let i = 0; i < ring.length; i++) {
+    let char = ring[i];
+    if (!positionsMap[char]) positionsMap[char] = [];
+    positionsMap[char].push(i);
   }
-  let memo = Array(n).fill(0).map(() => Array(m).fill(-1));
-  return dp(0, 0) + n;
+  let memo = Array(ring.length).fill(0).map(() => Array(key.length).fill(-1));
+  return dp(0, 0);
   
-  function dp(i, j) {
-    if (i === n) return 0;
-    if (memo[i][j] !== -1) return memo[i][j];
+  function dp(ringIndex, keyIndex) {
+    if (keyIndex === key.length) return 0;
+    if (memo[ringIndex][keyIndex] !== -1) return memo[ringIndex][keyIndex];
     
+    let positions = positionsMap[key[keyIndex]];
     let ans = Infinity;
-    for (let idx of indices[key.charCodeAt(i) - 97]) {
-      let cost = getMinCost(j, idx);
-      ans = Math.min(ans, dp(i + 1, idx) + cost);
+    for (let position of positions) {
+      let moves = getMinMoves(ringIndex, position, ring.length);
+      ans = Math.min(ans, 1 + moves + dp(position, keyIndex + 1));
     }
-    return memo[i][j] = ans;
-  }
-  
-  function getMinCost(i, j) {
-    if (i > j) return getMinCost(j, i);
-    return Math.min(j - i, i + m - j);
+    return memo[ringIndex][keyIndex] = ans;
   }
 };
+  
+function getMinMoves(position1, position2, n) {
+  if (position2 < position1) {
+    return getMinMoves(position2, position1, n);
+  }
+  return Math.min(n + position1 - position2, position2 - position1);
+}
 
-// Two test cases to run function on
+
+// Solution 2: Bottom-up DP & Space Optimized
+
+// Map the positions of occurances to each character: {g: [0,3,4], d: [1,2], ...}.
+
+// Since we are processing each character one after the other, we only need to store the minimum moves from the previous character.
+// For each key character, go through each new position, and go through each valid old position and calculate the minimum number of moves from the old to new positions.
+
+// n = length of ring, m = length of key
+// Time Complexity: O(n^2 * m) 83ms
+// Space Complexity: O(n) 55.8MB
+var findRotateSteps = function(ring, key) {
+  let positionsMap = {}, n = ring.length;
+  for (let i = 0; i < n; i++) {
+    let char = ring[i];
+    if (!positionsMap[char]) positionsMap[char] = [];
+    positionsMap[char].push(i);
+  }
+  let prev = Array(n).fill(Infinity);
+  prev[0] = 0;
+  for (let i = 0; i < key.length; i++) {
+    let curr = Array(n).fill(Infinity);
+    let positions = positionsMap[key[i]];
+    for (let position of positions) {
+      for (let j = 0; j < n; j++) {
+        if (prev[j] === Infinity) continue;
+        let moves = getMinMoves(position, j, n);
+        curr[position] = Math.min(curr[position], 1 + moves + prev[j]);
+      }
+    }
+    prev = curr;
+  }
+  return Math.min(...prev);
+};
+
+// Two test cases
 console.log(findRotateSteps("godding", "gd")) // 4
 console.log(findRotateSteps("godding", "godding")) // 13
