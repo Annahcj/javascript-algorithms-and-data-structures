d1ebdb117e6909e11498507143fda9c7a31b5e1b
Anna An <annaan.golf@gmail.com>
2024-05-21 21:15:51 +1200

    Add solution

--- Backtracking/78.subsets.js
+++ Backtracking/78.subsets.js
@@ -3,26 +3,30 @@
 // The solution set must not contain duplicate subsets. Return the solution in any order.
 
 
-// Solution: Backtracking 
+// Solution: Backtracking
 
-// Time Complexity: O(2^n) 84ms
-// Space Complexity: O(n) 40.8MB
+// Use recursive backtracking to generate every subset.
+// To avoid generating duplicates, sort nums in asc order, and use a pointer i to indicate which index we are up to.
+// For each index i, we either include nums[i] in our subset, or skip it.
+
+// Time Complexity: O(2^n * n) 60ms
+// Space Complexity: O(n) (excluding output) 50.8MB
 var subsets = function(nums) {
-  let res = [];
-  recurse(0, []);
-  return res;
-
-  function recurse(idx, arr) {
-    // create deep copy of arr
-    res.push([...arr]);
-    for (let i = idx; i < nums.length; i++) {
-      // push 
-      arr.push(nums[i]);
-      recurse(i + 1, arr);
-      // backtrack, pop off
-      arr.pop();
-    }
-  }  
+  nums.sort((a, b) => a - b);
+  let subsets = [], n = nums.length;
+  backtrack(0, []);
+  return subsets;
+  
+  function backtrack(i, subset) {
+    if (i === n) {
+      subsets.push([...subset]);
+      return;
+    };
+    subset.push(nums[i]);
+    backtrack(i + 1, subset); // take
+    subset.pop();
+    backtrack(i + 1, subset); // skip
+  }
 };
 
 // Two test cases
