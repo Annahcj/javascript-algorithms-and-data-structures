b54c44ef1020e6685ccb5194d9f27edf9cbb5466
Anna An <annaan.golf@gmail.com>
2024-06-07 21:52:34 +1200

    Add solution

--- Trie/648.replace-words.js
+++ Trie/648.replace-words.js
@@ -6,51 +6,55 @@
 
 // Solution: Trie
 
-// Add each dictionary word to a trie.
-// For each word in the sentence, find the shortest prefix of the word in the trie.
+// Store words from the dictionary in a trie.
+// For each word in sentence, find the first prefix match in the trie.
 
-// n = number of words in dictionary, m = number of words in sentence, k = maximum length of a word
-// Time Complexity: O(nk + mk) 505ms
-// Space Complexity: O(nk) 109.5MB
+// Using a trie is faster than a hashset, because you don't need to build up or create the prefix substring when we try to find the prefix match.
+// With a trie, you traverse each matching node, and when we find the first match we can then return the substring, just once.
+
+// n = words in dictionary, m = words in sentence, k = max length of a word
+// Time Complexity: O(nk + mk) 85ms
+// Space Complexity: O(nk + mk) 69.6MB
 var replaceWords = function(dictionary, sentence) {
   let trie = new Trie();
-  for (let word of dictionary) trie.add(word);
-  let words = sentence.split(" "), res = [];
-  for (let word of words) {
-    let firstPrefix = trie.firstPrefix(word);
-    res.push(firstPrefix);
+  for (let word of dictionary) {
+    trie.add(word);
   }
-  return res.join(" ");
+  let words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = trie.firstPrefix(words[i]);
+  }
+  return words.join(" ");
 };
 
 class TrieNode {
   constructor() {
     this.children = {};
-    this.isWordEnd = false;
+    this.isWordEnd = false; 
   }
 }
 
 class Trie {
   constructor() {
-    this.trie = new TrieNode();
+    this.root = new TrieNode();
   }
   add(word) {
-    let node = this.trie;
-    for (let char of word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
       node = node.children;
+      let char = word[i];
       if (!node[char]) node[char] = new TrieNode();
       node = node[char];
     }
     node.isWordEnd = true;
   }
   firstPrefix(word) {
-    let node = this.trie, prefix = "";
-    for (let char of word) {
-      prefix += char;
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
       node = node.children;
-      if (!node[char]) node[char] = new TrieNode();
-      node = node[char];
-      if (node.isWordEnd) return prefix;
+      if (!node[word[i]]) return word; // no matches - take the whole word
+      node = node[word[i]];
+      if (node.isWordEnd) return word.slice(0, i + 1);
     }
     return word;
   }
