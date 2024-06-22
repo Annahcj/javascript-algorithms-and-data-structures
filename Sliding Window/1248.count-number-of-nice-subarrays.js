82edb814227d16280d12eced050e97bc0bbebe66
Anna An <annaan.golf@gmail.com>
2024-06-22 22:01:18 +1200

    Add solution

--- Sliding Window/1248.count-number-of-nice-subarrays.js
+++ Sliding Window/1248.count-number-of-nice-subarrays.js
@@ -5,32 +5,35 @@
 
 // Solution: Sliding Window
 
-// Keep a count of possible subarrays, and ans.
-// Set left pointer to 0
-// Loop through nums (pointer = right)
-  // if nums[right] is an odd number, decrement k by one, set count to 0 (now the past count is disregarded)
-  // loop while k is equal to 0
-    // if nums[left] is odd, increment k by one
-    // increment count by one
-    // increment left by one
-  // increment ans by count
-// Return ans
+// Maintain a sliding window of k odd numbers.
+// When the count of odd numbers exceeds k, move the left pointer up.
 
-// Time Complexity: O(n) 100ms
-// Space Complexity: O(1) 48.8MB 
+// To account for subarrays that start with even numbers, count the number of even numbers on the left of the first odd number in the subarray.
+// This count determines the number of subarrays that would start with an even number, but still includes exactly k odd numbers.
+
+// n = length of nums
+// Time Complexity: O(n) 79ms
+// Space Complexity: O(1) 58.1MB
 var numberOfSubarrays = function(nums, k) {
-  let count = 0, ans = 0;
-  let left = 0;
-  for (let right = 0; right < nums.length; right++) {
-    if (nums[right] % 2 === 1) k--, count = 0;
-    while (k === 0) {
-      if (nums[left] % 2 === 1) k++;
-      count++;
-      left++;
+  let n = nums.length, odd = 0;
+  let leftEven = 0, subarrays = 0;
+  for (let j = 0, i = 0; j < n; j++) {
+    odd += nums[j] % 2 === 1 ? 1 : 0;
+    while (odd > k) {
+      odd -= nums[i] % 2 === 1 ? 1 : 0;
+      i++;
+      leftEven = 0; // reset even count on the left since we're moving the window up
     }
-    ans += count;
+    if (odd === k) {
+      // update the count of even numbers on the left side of the window
+      while (nums[i] % 2 === 0) {
+        leftEven++;
+        i++;
+      }
+      subarrays += leftEven + 1;
+    }
   }
-  return ans;
+  return subarrays;
 };
 
 // Three test cases 
