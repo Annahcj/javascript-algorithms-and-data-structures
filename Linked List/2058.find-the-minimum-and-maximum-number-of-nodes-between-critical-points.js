5ea66715e513661c63048689aca49dee24bae3dd
Anna An <annaan.golf@gmail.com>
2024-07-05 21:28:05 +1200

    Add solution

--- Linked List/2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
+++ Linked List/2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
@@ -1,7 +1,7 @@
 // 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points
 
 
-// Solution: Get Indexes
+// Solution 1: Get Indexes
 
 // Loop through head, keep track of the index of where we are, and push the index into an array everytime the conditions match.
 // If length of the index array is smaller than 2, return [-1, -1]
@@ -29,4 +29,33 @@ var nodesBetweenCriticalPoints = function(head) {
     min = Math.min(min, idxs[i] - idxs[i - 1]);
   }
   return [min, max];
+};
+
+
+// Solution 2: On the Fly - Space Optimized
+
+// Store the indices of the first critical node and the previous critical node.
+// Keep track of the minimum distance between two critical nodes and update this on the fly when we come across critical nodes.
+// At the end, the maximum distance is the last state of the previous critical node - first critical node.
+
+// Time Complexity: O(n) 141ms
+// Space Complexity: O(1) 77.4MB
+var nodesBetweenCriticalPoints = function(head) {
+  let firstCritical = null, prevCritical = null;
+  let i = 0, minDist = Infinity;
+  while (head.next?.next) {
+    let isMaxima = head.next.val > head.val && head.next.val > head.next.next.val;
+    let isMinima = head.next.val < head.val && head.next.val < head.next.next.val;
+    if (isMaxima || isMinima) {
+      if (prevCritical !== null) {
+        minDist = Math.min(minDist, i - prevCritical);
+      } else {
+        firstCritical = i;
+      }
+      prevCritical = i;
+    }
+    head = head.next;
+    i++;
+  }
+  return minDist === Infinity ? [-1, -1] : [minDist, prevCritical - firstCritical];
 };
