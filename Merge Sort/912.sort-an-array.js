aba1e8ff18f51d784e37e6a28a89d6429bac2620
Anna An <annaan.golf@gmail.com>
2024-07-25 21:00:50 +1200

    Add solution

--- Merge Sort/912.sort-an-array.js
+++ Merge Sort/912.sort-an-array.js
@@ -2,42 +2,53 @@
 // Given an array of integers nums, sort the array in ascending order.
 
 
-var sortArray = function(nums) {
-  mergeSort(nums);
-  return nums;
-};
+// Solution 1: Quick Sort
 
-// Solution 1: Quick Sort 
+// Recursively use the partition function on the split segments of the array (within the start and end indexes).
+// Partition function - picks a pivot value and moves all smaller values to the left of the pivot, and all larger values to the right of the pivot:
+  // 1. Choose the middle index as the pivot value.
+  // 2. Move the pivot to the end.
+  // 3. Compare all numbers against the pivot value and swap it with the pivot index and increment the index if smaller than the pivot value.
+  // 4. Move the pivot value to its final position (the final state of the pivot index).
 
-// Time Complexity: O(n log(n)) 223ms
-// Space Complexity: O(n) 49.6MB
-function quickSort(nums, start = 0, end = nums.length - 1) {
-  if (start > end) return;
-  let idx = partition(nums, start, end);
-  quickSort(nums, start, idx - 1);
-  quickSort(nums, idx + 1, end);
+// In summary, we're using divide and conquer to recursively "pivot sort" each segment of the array until the whole array is sorted.
+// The time complexity is O(n log(n)) because we split each segment of the array into 2 at each step (log(n) times), with the partition function taking O(n).
+
+// Time Complexity: O(n log(n)) 2424ms
+// Space Complexity: O(log(n)) 75.7MB
+function sortArray(nums, start = 0, end = nums.length - 1) {
+  if (start >= end) return nums;
+  let pivotIndex = partition(nums, start, end);
+  sortArray(nums, start, pivotIndex - 1);
+  sortArray(nums, pivotIndex + 1, end);
   return nums;
-};
+}
 
 function partition(nums, start, end) {
-  let midIdx = Math.floor((start + end) / 2);
-  let pivotVal = nums[midIdx];
-  let pivotIdx = start;
-  [nums[midIdx], nums[end]] = [nums[end], nums[midIdx]];
-  for (var i = start; i < end; i++) {
-    if (nums[i] < pivotVal) {
-      [nums[i], nums[pivotIdx]] = [nums[pivotIdx], nums[i]];
-      pivotIdx++;
+  let midIndex = Math.floor((start + end) / 2);
+  let pivotValue = nums[Math.floor((start + end) / 2)];
+  [nums[midIndex], nums[end]] = [nums[end], nums[midIndex]];
+  let pivotIndex = start;
+  for (let i = start; i < end; i++) {
+    if (nums[i] < pivotValue) {
+      [nums[i], nums[pivotIndex]] = [nums[pivotIndex], nums[i]];
+      pivotIndex++;
     }
   }
-  [nums[pivotIdx], nums[end]] = [nums[end], nums[pivotIdx]];
-  return pivotIdx;
+  [nums[end], nums[pivotIndex]] = [nums[pivotIndex], nums[end]];
+  return pivotIndex;
 }
+
 
 // Solution 2: Merge Sort
 
 // Time Complexity: O(n log(n)) 152ms
 // Space Complexity: O(n) 52.1MB
+var sortArray = function(nums) {
+  mergeSort(nums);
+  return nums;
+};
+
 function mergeSort(nums, start = 0, end = nums.length - 1) {
   if (start >= end) return;
   let mid = Math.floor((start + end) / 2);
