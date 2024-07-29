0938cda694b8ec4b5b591ffb68131c1f53d32843
Anna An <annaan.golf@gmail.com>
2024-07-29 22:13:36 +1200

    Add solution

--- Counting/1395.count-number-of-teams.js
+++ Counting/1395.count-number-of-teams.js
@@ -6,7 +6,7 @@
 // Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
 
 
-// Solution: Count in Left & Right
+// Solution 1: Count in Left & Right
 
 // Calculate combinations with j as middle element, find:
   // smaller on the left, bigger on the right
@@ -36,6 +36,69 @@ var numTeams = function(rating) {
   }
   return res;
 };
+
+
+// Solution 2: Segment Tree
+
+// Anchor at the middle index j.
+// The number of teams = 
+  // smallerLeft[j] * greaterRight[j]
+  // greaterLeft[j] * smallerRight[j]
+
+// For each index j, calculate the smaller and greater ratings on the left and right of j.
+// We can find the number of greater or smaller elements using a segment tree.
+
+// n = length of rating, m = max(rating[i])
+// Time Complexity: O(m + n log(m)) 76ms
+// Space Complexity: O(m) 57.8MB
+var numTeams = function(rating) {
+  let n = rating.length, max = Math.max(...rating);
+  let smallerRight = Array(n).fill(0);
+  let greaterRight = Array(n).fill(0);
+  let segTree = new SegmentTree(max + 1);
+  for (let j = n - 1; j >= 0; j--) {
+    smallerRight[j] = segTree.sumRange(0, rating[j] - 1);
+    greaterRight[j] = segTree.sumRange(rating[j] + 1, max);
+    segTree.update(rating[j], 1);
+  }
+  segTree = new SegmentTree(max + 1);
+  let teams = 0;
+  for (let j = 0; j < n; j++) {
+    let smallerLeft = segTree.sumRange(0, rating[j] - 1);
+    let greaterLeft = segTree.sumRange(rating[j] + 1, max);
+    teams += smallerLeft * greaterRight[j] + greaterLeft * smallerRight[j];
+    segTree.update(rating[j], 1);
+  }
+  return teams;
+};
+
+class SegmentTree {
+  constructor(n) {
+    this.size = n;
+    this.segTree = Array(n * 2).fill(0);
+  }
+  update(index, val) {
+    let n = this.size, idx = index + n;
+    this.segTree[idx] = val;
+    idx = Math.floor(idx / 2);
+
+    while (idx > 0) {
+      this.segTree[idx] = this.segTree[idx * 2] + this.segTree[idx * 2 + 1];
+      idx = Math.floor(idx / 2);
+    }
+  }
+  sumRange(left, right) {
+    let n = this.size, sum = 0;
+    let left_idx = left + n, right_idx = right + n;
+    while (left_idx <= right_idx) {
+      if (left_idx % 2 === 1) sum += this.segTree[left_idx++];
+      if (right_idx % 2 === 0) sum += this.segTree[right_idx--];
+      left_idx = Math.floor(left_idx / 2);
+      right_idx = Math.floor(right_idx / 2);
+    }
+    return sum;
+  }
+}
 
 // Three test cases
 console.log(numTeams([2,5,3,4,1])) // 3
