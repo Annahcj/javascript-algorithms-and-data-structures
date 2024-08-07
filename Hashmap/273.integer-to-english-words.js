608d4c310bd61a68f691e876865ef15079c48839
Anna An <annaan.golf@gmail.com>
2024-08-07 21:05:18 +1200

    Add solution

--- Hashmap/273.integer-to-english-words.js
+++ Hashmap/273.integer-to-english-words.js
@@ -1,106 +1,91 @@
 // 273. Integer to English Words
 // Convert a non-negative integer num to its English words representation.
 
-// Solution: Using a Map to store English Translations
 
-// Thoughts:
-// We store numbers from 1 to 20, 30 to 90 (going up in tens each time), and a thousand, a million, and a billion.
-// We can store numbers under a hundred in a hashmap (key = number, value = english) for efficient lookup, and the three cases above a thousand in an array in the following format [number, english].
+// Solution:
 
-// Algorithm:
-// helper function toWord: (accepts a number under 1000, keep track of str to store english translation)
-  // If num is 0, return str.
-  // If num is smaller than 20, return the current str + its translation in the u100 hashmap.
-  // If num is smaller than 100
-    // Find translation of the (first digit of num) * 10, add it to str.
-    // Get rid of the first digit of num.
-    // Recursively call toWord for num.
-  // If num is smaller than 1000
-    // Find translation of the first digit of num, add it + 'Hundred' to str.
-    // Get rid of the first digit of num.
-    // Recursively call toWord for num.
-// numerToWords:
-// Make a hashmap 'u100' and put the english word for numbers 1 to 20, 30, 40, 50, 60, 70, 80, and 90.
-// Make an array 'thousand' and put translations for a thousand, a million, and a billion.
-// Edge case: If num is 0, return 'Zero'.
-// Keep an answer string 'ans'
-// Loop while number is bigger than 0
-  // If length of ans is bigger than 0, add a space to it. 
-  // If num is smaller than 1000, return ans + toWord(num)
-  // Otherwise, loop through thousand and find the first number that is smaller than or equal to num, save it in a variable 'word'. (If num is 2000, stop at 1000)
-  // Find next (thousand/million/billion) number, let's call it 'int'. (say if the number is 12543, we find 12). We can calculate this by doing parseInt(num / (thousand/million/billion)). parseInt gets rid of decimal points. 
-  // Find the remaining number, set num to num % (thousand/million/billion).
-  // Add toWord(int) + (thousand/million/billion) to ans.
-// When all is finished, return ans.
+// Split num in segments of 3, starting from the end.
+// Each segment is less than 1000 and will have the same prefix (123 = One Hundred Twenty Three) regardless of the suffix (Thousand, Million, Billion).
 
-// Time Complexity: O(n) (where n is the length of the digits of num) 80ms
-// Space Complexity: O(1) (although we store words in a hashmap, it is constant and never changes) 40.2MB
-  var numberToWords = function(num) {
-    let u100 = {
-      1: 'One',
-      2: 'Two',
-      3: 'Three',
-      4: 'Four',
-      5: 'Five',
-      6: 'Six',
-      7: 'Seven',
-      8: 'Eight',
-      9: 'Nine',
-      10: 'Ten',
-      11: 'Eleven',
-      12: 'Twelve',
-      13: 'Thirteen',
-      14: 'Fourteen',
-      15: 'Fifteen',
-      16: 'Sixteen',
-      17: 'Seventeen',
-      18: 'Eighteen',
-      19: 'Nineteen',
-      20: 'Twenty',
-      30: 'Thirty',
-      40: 'Forty',
-      50: 'Fifty',
-      60: 'Sixty',
-      70: 'Seventy',
-      80: 'Eighty',
-      90: 'Ninety'
-    };
-    let thousand = [[1000, 'Thousand'], [1000000, 'Million'], [1000000000, 'Billion']];
-    if (num === 0) return 'Zero';
-    let ans = '';
-    while (num > 0) {
-      if (ans.length > 0) ans += ' ';
-      if (num < 1000) return ans + toWord(num);
-      for (var i = 2; i >= 0; i--) {
-        if (thousand[i][0] <= num) {
-          break;
-        }
-      }
-      let word = thousand[i][1];
-      let remaining = num % thousand[i][0];
-      let int = parseInt(num / thousand[i][0]);
-      ans += `${toWord(int)} ${word}`;
-      num = remaining;
-    }
-    return ans;
-    function toWord(num, str='') {
-      if (num === 0) return str;
-      if (str.length > 0) str += ' ';
-      if (num < 20) return str += `${u100[num]}`;
-      if (num < 100) {
-        let int = parseInt(num / 10) * 10;
-        return toWord(num - int, str + `${u100[int]}`);
-      } else {
-        let int = parseInt(num / 100);
-        return toWord(num - int * 100, str + `${u100[int]} Hundred`);
-      }
-    }
+// Special cases:
+  // 1. If the whole number is 0, return 'Zero'.
+  // 2. If a segment starts with 0, ignore all the leading zeros (e.g. '092' or '007').
+  // 3. If a segment is '000', omit it from the final word representation (e.g. 123000 -> 'One Hundred Twenty Three Thousand').
+  // 4. Numbers from 10 to 19 need a special mapping that ignores the last digit.
+
+// Time Complexity: O(1) 61ms
+// Space Complexity: O(1) 51.4MB
+var numberToWords = function(num) {
+  if (num === 0) return 'Zero';
+  num = num.toString();
+  const suffix = ['', ' Thousand', ' Million', ' Billion'];
+  let words = [], n = num.length;
+  for (let i = n - 1, suffixIndex = 0; i >= 0; i -= 3, suffixIndex++) {
+    let startIndex = Math.max(0, i - 2);
+    let prefix = getEnglishWordForSegment(num.slice(startIndex, i + 1));
+    if (prefix === 'Zero') continue; // skip all zeros
+    words.push(prefix + suffix[suffixIndex]);
+  }
+  return words.reverse().join(" ");
+};
+
+// Return the english word for the number (< 1000).
+function getEnglishWordForSegment(num) {
+  num = parseInt(num).toString();
+  if (num === '0') return 'Zero';
+  const onesMap = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine'
+  };
+  const teensMap = {
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen'
+  };
+  const tensMap = {
+    2: 'Twenty',
+    3: 'Thirty',
+    4: 'Forty',
+    5: 'Fifty',
+    6: 'Sixty',
+    7: 'Seventy',
+    8: 'Eighty',
+    9: 'Ninety'
   };
   
-  // Six test cases to run function on
-  console.log(numberToWords(1000)) // One Thousand
-  console.log(numberToWords(0)) // Zero
-  console.log(numberToWords(123)) // "One Hundred Twenty Three"
-  console.log(numberToWords(12345)) // "Twelve Thousand Three Hundred Forty Five"
-  console.log(numberToWords(1234567)) // "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
-  console.log(numberToWords(1234567891)) // "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+  let word = [];
+  if (num.length === 3) { // in the hundreds
+    word.push(onesMap[num[0]] + ' Hundred');
+  }
+  if (num.length >= 2) {
+    if (teensMap[num.slice(num.length - 2)]) { // special mapping for 10-19
+      word.push(teensMap[num.slice(num.length - 2)]);
+      return word.join(" ");
+    } else if (num[num.length - 2] !== '0') { // omit trailing zeros
+      word.push(tensMap[num[num.length - 2]]); 
+    }
+  }
+  if (num[num.length - 1] !== '0') { // last digit
+    word.push(onesMap[num[num.length - 1]]);
+  }
+  return word.join(" ");
+}
+
+// Three test cases
+console.log(numberToWords(123)) // "One Hundred Twenty Three"
+console.log(numberToWords(12345)) // "Twelve Thousand Three Hundred Forty Five"
+console.log(numberToWords(1234567)) // "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
