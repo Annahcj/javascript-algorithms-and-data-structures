8272b9ac18274b78a5bc9c4e7ce92e2635a35bb1
Anna An <annaan.golf@gmail.com>
2024-09-26 23:08:25 +0900

    Add solution

---
+++ Sliding Window/3298.count-substrings-that-can-be-rearranged-to-contain-a-string-II.js
@@ -0,0 +1,50 @@
+// 3298. Count Substrings That Can Be Rearranged to Contain a String II
+// You are given two strings word1 and word2.
+// A string x is called valid if x can be rearranged to have word2 as a prefix.
+// Return the total number of valid substrings of word1.
+
+
+// Solution: Sliding Window
+
+// Count the occurances of each character in word2.
+// Maintain a minimum-lengthed sliding window of matching characters in word1.
+// The right pointer moves up incrementally and the left pointer moves up while all characters in word2 exist in word1.
+// For each valid window with all matching characters, the number of valid substrings is equal to the index of the left pointer + 1 (because if all characters exist in the window, extending the window to the left will still contain all those characters).
+
+// n = length of word1, m = length of word2
+// Time Complexity: O(m + n) 163ms
+// Space Complexity: O(1) 65.6MB
+function validSubstringCount(word1, word2) {
+  let count = Array(26).fill(0);
+  let distinct = 0;
+  for (let char of word2) {
+    count[char.charCodeAt() - 97]++;
+    if (count[char.charCodeAt() - 97] === 1) {
+      distinct++;
+    }
+  }
+  let matching = 0, substrings = 0;
+  for (let j = 0, i = 0; j < word1.length; j++) {
+    count[word1.charCodeAt(j) - 97]--;
+    if (count[word1.charCodeAt(j) - 97] === 0) {
+      matching++;
+    }
+    while (matching >= distinct) {
+      if (count[word1.charCodeAt(i) - 97] === 0) {
+        if (matching === distinct) break;
+        matching--;
+      }
+      count[word1.charCodeAt(i) - 97]++;
+      i++;
+    }
+    if (matching === distinct) {
+      substrings += i + 1;
+    }
+  }
+  return substrings;
+};
+
+// Three test cases
+console.log(validSubstringCount("bcca", "abc")) // 1
+console.log(validSubstringCount("abcabc", "abc")) // 10
+console.log(validSubstringCount("abcabc", "aaabc")) // 0
