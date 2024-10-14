c8a290c6db9f3573a960abaccc5a04899b736bbb
Anna An <annaan.golf@gmail.com>
2024-10-14 22:37:53 +1300

    Add solution

--- Dynamic Programming/3316.find-maximum-removals-from-source-string.js
+++ Dynamic Programming/3316.find-maximum-removals-from-source-string.js
@@ -7,7 +7,7 @@
 // Return the maximum number of operations that can be performed.
 
 
-// Solution: DP - Recursion w/ Memoization
+// Solution 1: DP - Recursion w/ Memoization
 
 // Memoize each dp(i, j), where
   // i = index in source
@@ -39,6 +39,38 @@ var maxRemovals = function(source, pattern, targetIndices) {
     }
     return memo[i][j] = ans;
   }
+};
+
+
+// Solution 2: Iterative DP - Bottom Up
+
+// Iterative bottom up DP.
+// We only need to know the maximum removals for the previous index in source.
+// Keep track of the previous and current DP results.
+
+// prev[j] = Maximum removals matching up to j characters in pattern.
+
+// n = length of source, m = length of pattern
+// Time Complexity: O(nm) 174ms
+// Space Complexity: O(m) 56.2MB
+var maxRemovals = function(source, pattern, targetIndices) {
+  let n = source.length, m = pattern.length;
+  targetIndices = new Set(targetIndices);
+  let prev = Array(m + 1).fill(-Infinity);
+  prev[0] = 0;
+  for (let i = 0; i < n; i++) {
+    let curr = Array(m + 1).fill(-Infinity);
+    let isTargetIndex = targetIndices.has(i);
+    curr[0] = Math.max(curr[0], prev[0] + (isTargetIndex ? 1 : 0));
+    for (let j = 0; j < m; j++) {
+      curr[j + 1] = Math.max(curr[j + 1], prev[j + 1] + (isTargetIndex ? 1 : 0));
+      if (source[i] === pattern[j]) {
+        curr[j + 1] = Math.max(curr[j + 1], prev[j]);
+      }
+    }
+    prev = curr;
+  }
+  return prev[m];
 };
 
 // Four test cases
