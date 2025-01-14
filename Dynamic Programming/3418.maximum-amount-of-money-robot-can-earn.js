905eb433057a6275eb8608c89600ab079cc3bdbb
Anna An <annaan.golf@gmail.com>
2025-01-14 22:45:32 +1300

    Add solution

--- Dynamic Programming/3418.maximum-amount-of-money-robot-can-earn.js
+++ Dynamic Programming/3418.maximum-amount-of-money-robot-can-earn.js
@@ -8,7 +8,7 @@
 // Return the maximum profit the robot can gain on the route.
 
 
-// Solution: DP - Recursion w/ Memoization
+// Solution 1: DP - Recursion w/ Memoization
 
 // Memoize each dp(i, j, k), where
   // i = current row
@@ -42,6 +42,48 @@ var maximumAmount = function(coins) {
     } 
     return memo[i][j][k] = maxCoins;
   }
+};
+
+
+// Solution 2: Bottom Up Iterative DP
+
+// Observe that we only need results from the previous row and current row.
+// curr[j][k] = maximum profit reaching cell (current row, j), where k robbers have been neutralized.
+// prev is the results from the previous row.
+
+// For every coins[i][j], go through every amount of robbers that we neutralize (0 to 2).
+// There are at most four scenarios, take the maximum out of the four:
+  // 1. The path from directly above + not neutralizing 
+  // 2. The path from directly above + neutralizing if k > 0 and coins[i][j] is negative.
+  // 3. The path from directly left + not neutralizing.
+  // 4. The path from directly left + neutralizing if k > 0 and coins[i][j] is negative.
+
+// m = number of rows, n = number of columns
+// Time Complexity: O(3 * mn) 347ms
+// Space Complexity: O(3n) 77.58MB
+var maximumAmount = function(coins) {
+  const m = coins.length, n = coins[0].length;
+  let prev = Array(n).fill(0).map(() => Array(3).fill(-Infinity));
+  prev[0][0] = 0;
+  for (let i = 0; i < m; i++) {
+    const curr = Array(n).fill(0).map(() => Array(3).fill(-Infinity));
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k <= 2; k++) {
+        curr[j][k] = Math.max(curr[j][k], coins[i][j] + prev[j][k]);
+        if (coins[i][j] < 0 && k > 0) {
+          curr[j][k] = Math.max(curr[j][k], prev[j][k - 1]);
+        }
+        if (j > 0) {
+          curr[j][k] = Math.max(curr[j][k], coins[i][j] + curr[j - 1][k]);
+          if (coins[i][j] < 0 && k > 0) {
+            curr[j][k] = Math.max(curr[j][k], curr[j - 1][k - 1]);
+          }
+        }
+      }
+    }
+    prev = curr;
+  }
+  return Math.max(...prev[n - 1]);
 };
 
 // Two test cases
