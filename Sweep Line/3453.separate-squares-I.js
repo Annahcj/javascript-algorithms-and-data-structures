e81049308668a750a7a456b9d6f7120052a665c1
Anna An <annaan.golf@gmail.com>
2025-02-22 22:17:56 +1300

    Add solution

--- Sweep Line/3453.separate-squares-I.js
+++ Sweep Line/3453.separate-squares-I.js
@@ -7,6 +7,21 @@
 
 // Solution: Line Sweep
 
+// Use line sweep the find the running sum of areas.
+// For every square, 
+  // [y, length] to add to the current width
+  // [y + length, -length] to subtract off the width
+// Sort the widths in asc order by y to perform the line sweep.
+
+// Keep track of the current width, the previous y, and the current sum of areas.
+// The area for the current block = the difference between the current y and the previous y, multiplied by the current width.
+
+// Find the smallest y where the sum of area >= target area.
+// Formula to calculate the decimal amount ratio to add to the previous y:
+  // The missing area = Difference between the target sum and the previous area (area sum for the previous y).
+  // The ratio to add to the previous y (the smaller one) = the missing area / width for the current y.
+    // We divide by the width for the current y (1 y, not the whole area), because the ratio needs to be in relation to 1 y.
+
 // length of squares
 // Time Complexity: O(n log(n)) 335ms
 // Space Complexity: O(n) 98.08MB
